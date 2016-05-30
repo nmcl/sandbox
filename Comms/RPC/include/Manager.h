ae14a5e972da8c59ea675bba0ed33d2b7a9cc289
Mark Little <mlittle@redhat.com>
2016-05-30 12:30:59 +0100

    Early C++ RPC for teaching.
    https://github.com/nmcl/sandbox/issues/9

---
+++ Comms/RPC/include/Manager.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 1990, 1991,
+ * Computing Laboratory, University of Newcastle upon Tyne, UK.
+ *
+ * Permission to use, copy, modify and distribute this program for
+ * evaluation, teaching and/or research purposes only and without fee is
+ * hereby granted, providing that this copyright and permission notice
+ * appear on all copies and supporting documentation, and that similar
+ * conditions are imposed on any individual or organization to whom the
+ * program is distributed. The University of Newcastle upon Tyne makes no
+ * representation about the suitability of this software for any purpose.
+ * It is provided "as is" without express or implied warranty.
+ */
+
+#ifndef _MANAGER_H_
+#define _MANAGER_H_
+
+/*
+ *  $Id: Manager.h,v 1.1 1991/08/19 19:49:51 nsmw Exp $
+ */
+
+#define MANAGER_PORT 5001
+
+enum managerOpcodes { Do_Create_Server, Do_Quit };
+
+#endif  _MANAGER_H_
