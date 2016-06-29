10530bfbcba4e8a80a6b4d1ba0a78b07d95f16fb
Mark Little <mlittle@redhat.com>
2016-06-29 14:20:54 +0100

    https://github.com/nmcl/sandbox/issues/29

---
+++ TatsuII/Common/ErrorListI.cc
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: ErrorListI.cc,v 1.1 1997/06/09 19:51:47 nmcl Exp $
+ */
+
+/*
+ *
+ * Error iterator class
+ *
+ */ 
+
+#ifndef ERRORLIST_H_
+#  include <Common/ErrorList.h>
+#endif
+
+#ifndef ERRORLISTI_H_
+#  include <Common/ErrorListI.h>
+#endif
+
+#ifndef ERROR_H_
+#  include <Common/Error.h>
+#endif
+
+static const char RCSid[] = "$Id: ErrorListI.cc,v 1.1 1997/06/09 19:51:47 nmcl Exp $";
+
+/*
+ * Public constructors and destructors 
+ */
+
+ErrorListI::ErrorListI ( const ErrorList& L )
+		       : currentList(L),
+			 next(L.head)
+{ 
+}
+
+ErrorListI::~ErrorListI ()
+{
+}
+
+const Error *const ErrorListI::operator() ()
+{
+    Error *current = next;
+    if (current == 0)
+    {
+	return 0;
+    }
+    else
+	next = current->getLink();
+
+    return current;
+}
+
+#ifdef NO_INLINES
+#  define ERRORLISTI_CC_
+#  include <Common/ErrorListI.n>
+#  undef ERRORLISTI_CC_
+#endif
---
+++ TatsuII/Common/Filtsbuf.cc
@@ -0,0 +1,152 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: Filtsbuf.cc,v 1.1 1997/06/09 19:51:47 nmcl Exp $
+ */
+
+/*
+ *
+ * Simple filtering streambuffer. On overflow and underflow the buffer
+ * is handed to a filtering class which may do with it as it sees fit.
+ * Code based on example int eh 2.1 / 3.0 iostream manuals
+ */
+
+#ifndef IOSTREAM_H_
+#  include <System/iostream.h>
+#endif
+
+#ifndef FILTSBUF_H_
+#  include <Common/Filtsbuf.h>
+#endif
+
+#ifndef zapeof
+#  define zapeof(c) ((c)&0377)
+#endif
+
+static const char RCSid[] = "$Id: Filtsbuf.cc,v 1.1 1997/06/09 19:51:47 nmcl Exp $";
+
+#ifdef NO_NESTED_TYPES
+Filterbuf::Filterbuf ( StreamFilter& cl, open_mode m )
+#else
+Filterbuf::Filterbuf ( StreamFilter& cl, ios::open_mode m )
+#endif
+                     : streambuf(), 
+		       filter(cl),
+		       mode(m)
+{
+}
+
+Filterbuf::~Filterbuf ()
+{
+}
+
+int Filterbuf::overflow ( int c )
+{
+    /* check that output is allowed */
+    if (!(mode & ios::out))
+	return EOF;
+    
+    /* make sure there is a holding area */
+    if (allocate() == EOF)
+	return EOF;
+    
+    /* verify that there are no characters in get area */
+    if (gptr() && gptr() < egptr())
+	return EOF;
+    
+    /* reset get area */
+    setg(0, 0, 0);
+    
+    /* make sure there is a put area */
+    if (!pptr()) setp(base(), base());
+    
+    /* determine how many characters have been inserted but not consumed */
+    int w = pptr() - pbase();
+    
+    /* if c is not EOF it is a character that must also be consumed */
+    if (c != EOF)
+    {
+	/* we always leave space */
+	*pptr() = c;
+	++w;
+    }
+    
+    /* consume characters */
+    int ok = filter.filter(pbase(), w, ios::out);
+    
+    if (ok)
+    {
+	/* set up put area. Ensure there is space for one extra character */
+	setp(base(), ebuf() - 1);
+	return zapeof(c);
+    }
+    else
+    {
+	/* indicate error. */
+	setp(0, 0);
+	return EOF;
+    }
+}
+
+int Filterbuf::underflow ()
+{
+    /* check that input is allowed */
+    if (!(mode & ios::in))
+	return EOF;
+    
+    /* make sure there is a holding area. */
+    if (allocate() == EOF)
+	return EOF;
+    
+    /* if there are characters waiting for output, then send them. */
+    if (pptr() && pptr() > pbase())
+	overflow(EOF);
+    
+    /* reset put area */
+    setp(0, 0);
+    
+    /* setup get area */
+
+    int blength = blen();
+
+    if (blength > 1)
+	setg(base(), base() + 1, ebuf());
+    else
+	setg(base(), base(), ebuf());
+    
+    /* produce characters */
+    int ok = filter.filter(base(), blength, ios::in);
+    
+    if (ok)
+	return zapeof(*base());
+    else
+    {
+	setg(0, 0, 0);
+	return EOF;
+    }
+}
+
+int Filterbuf::sync ()
+{
+    if (gptr() && egptr() > gptr())
+	/* no way to return characters to producer */
+	return EOF;
+
+    if (pptr() && pptr() > pbase())
+    {
+	/* flush waiting output */
+	int c = overflow(EOF);
+
+	if (c == EOF)
+	    return EOF;
+    }
+    
+    /* nothing to do */
+    return 0;
+}
+
