b22e8597358699e55bbda93c5b4916725fdd924c
Mark Little <mlittle@redhat.com>
2016-06-29 14:21:38 +0100

    https://github.com/nmcl/sandbox/issues/29

---
+++ TatsuII/Include/Common/ProcessMutex.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: ProcessMutex.h,v 1.1 1997/06/09 19:51:49 nmcl Exp $
+ */
+
+#ifndef PROCESSMUTEX_H_
+#define PROCESSMUTEX_H_
+
+/*
+ * Basic Process level Mutex implementation.
+ * Implements recursive process mutexes using SystemV semphores.
+ */
+
+#ifndef SYS_TYPES_H_
+#  include <System/sys/types.h>
+#endif
+
+#ifndef COMMONT_H_
+#  include <Common/CommonT.h>
+#endif
+
+#ifndef SEMAPHORE_H_
+#  include <Common/Semaphore.h>
+#endif
+
+class ProcessMutex
+{
+    friend class MutexList;
+    
+public:
+    static ProcessMutex *create (key_t);
+    static void destroy (ProcessMutex*&);
+    
+    /* classic semaphore operations */
+
+    Semaphore::Status signal ();
+    Semaphore::Status trywait ();
+    Semaphore::Status wait ();
+private:
+    ProcessMutex (key_t);
+    ~ProcessMutex ();
+
+    key_t mutexKey;
+    Semaphore *theSem;
+    int waitCount;
+};
+
+#endif
---
+++ TatsuII/Include/Common/Semaphore.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: Semaphore.h,v 1.1 1997/06/09 19:51:49 nmcl Exp $
+ */
+
+#ifndef SEMAPHORE_H_
+#define SEMAPHORE_H_
+
+/*
+ *
+ * Semaphore class interface description. Provide simple interface to
+ * System V semaphore handling routines.
+ *
+ */
+
+#ifndef SYS_TYPES_H_
+#  include <System/sys/types.h>
+#endif
+
+#ifndef SYS_IPC_H_
+#  include <System/sys/ipc.h>
+#endif
+
+#ifndef COMMONT_H_
+#  include <Common/CommonT.h>
+#endif
+
+class Semaphore
+{
+public:
+    enum Status { SEM_LOCKED, SEM_UNLOCKED, SEM_BLOCKED, SEM_ERROR };
+    
+    
+    /* Constructors and destructor */
+
+    Semaphore (const key_t, unsigned int = 1);
+    ~Semaphore ();
+    
+    /* classic semaphore operations */
+
+    Status signal ();
+    Status trywait ();
+    Status wait ();		      
+
+private:
+    /* non-virtual member function */
+
+    Boolean semCreate ();
+    
+    /* private state variables */
+
+    unsigned int initVal;
+    key_t semKey;
+    int semId;				/* the id as known by the system */
+};
+
+#endif
