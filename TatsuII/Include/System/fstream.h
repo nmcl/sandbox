085b09417881b00f0b9b0e4a738e09330e2c1785
Mark Little <mlittle@redhat.com>
2016-06-29 14:22:51 +0100

    https://github.com/nmcl/sandbox/issues/29

---
+++ TatsuII/Include/System/fstream.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: fstream.h,v 1.1 1997/06/09 19:51:58 nmcl Exp $
+ */
+
+#ifndef FSTREAM_H_
+#define FSTREAM_H_
+
+#ifdef STUB
+#  pragma @NoRename
+#endif
+
+// @NoRemote @NoMarshall
+class filebuf;
+
+// @NoRemote @NoMarshall
+class fstreambase;
+
+// @NoRemote @NoMarshall
+class ifstream;
+
+// @NoRemote @NoMarshall
+class ofstream;
+
+// @NoRemote @NoMarshall
+class fstream;
+
+#include <fstream.h>
+
+#endif
