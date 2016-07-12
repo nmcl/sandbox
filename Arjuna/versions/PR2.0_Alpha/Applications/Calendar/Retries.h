0426452766f6ac9b55ef862f02fb34c559669891
Mark Little <mlittle@redhat.com>
2016-07-12 20:43:30 +0100

    Finish Calendar application.
    https://github.com/nmcl/sandbox/issues/8

---
+++ Arjuna/versions/PR2.0_Alpha/Applications/Calendar/Retries.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ */
+
+
+/*
+ * $Id: Retries.h,v 1.2 1993/03/18 13:50:54 nmcl Exp $
+ */
+
+/* number of retries to obtain a lock       */
+/* used from within calendar & appointments */
+
+#define RETRIES 3
+
