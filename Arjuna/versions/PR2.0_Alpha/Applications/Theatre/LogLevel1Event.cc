94837bbb52ac0d6867052a4e505ad1411cda443d
Mark Little <mlittle@redhat.com>
2016-07-12 20:52:49 +0100

    Initial Thread application.
    https://github.com/nmcl/sandbox/issues/8

---
+++ Arjuna/versions/PR2.0_Alpha/Applications/Theatre/LogLevel1Event.cc
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ */
+
+
+/*
+ * $Id: LogLevel1Event.cc,v 1.2 1993/03/18 14:03:08 nmcl Exp $
+ */
+
+#include <InterViews/canvas.h>
+#include <InterViews/2.6/InterViews/shape.h>
+#include <InterViews/2.6/InterViews/painter.h>
+
+#include "LogInter.h"
+#include "LogLevel1Event.h"
+
+LogLevel1Event::LogLevel1Event(enum level1EventType et)
+{
+    eventType  = et;
+
+    shape->Rect(4*unitSize, 10*unitSize);
+    shape->Rigid();
+    output = new Painter;
+}
+
+LogLevel1Event::~LogLevel1Event()
+{
+}
+
+void LogLevel1Event::Redraw(Coord x1, Coord y1, Coord x2, Coord y2)
+{
+    output->Line (GetCanvas(),     0,            6*unitSize - 1, 4*unitSize - 1, 6*unitSize - 1);
+    switch (eventType)
+    {
+    case L1BeginEvent:
+	output->Line   (GetCanvas(), 0,   unitSize, 0,  2*unitSize -1);
+	output->Line   (GetCanvas(), (unitSize / 2),   unitSize, 0,  unitSize);
+	output->Text   (GetCanvas(), "Top-Level", (unitSize / 2), unitSize -1);
+	output->Text   (GetCanvas(), "Begin",     (unitSize / 2), 0);
+	output->Ellipse(GetCanvas(), 4*unitSize, 6*unitSize - 1, 4*unitSize, 3*unitSize);
+	break;
+    case L1CommitEvent:
+	output->Line   (GetCanvas(), 4*unitSize - 1,   unitSize, 4*unitSize - 1,  2*unitSize -1);
+	output->Line   (GetCanvas(), (unitSize / 2) + 3*unitSize - 1,   unitSize, 4*unitSize - 1,  unitSize);
+	output->Text   (GetCanvas(), "Top-Level",             (unitSize / 2), unitSize -1);
+	output->Text   (GetCanvas(), "Commit",                (unitSize / 2), 0);
+	output->Ellipse(GetCanvas(), 0,          6*unitSize - 1, 4*unitSize - 1, 3*unitSize);
+	break;
+    case L1AbortEvent:
+	output->Line   (GetCanvas(), 4*unitSize - 1,   unitSize, 4*unitSize - 1,  2*unitSize -1);
+	output->Line   (GetCanvas(), (unitSize / 2) + 3*unitSize - 1,   unitSize, 4*unitSize - 1,  unitSize);
+	output->Text   (GetCanvas(), "Top-Level",             (unitSize / 2), unitSize -1);
+	output->Text   (GetCanvas(), "Abort ",                (unitSize / 2), 0);
+	output->Ellipse(GetCanvas(), 0,          6*unitSize - 1, 4*unitSize - 1, 3*unitSize);
+	break;
+    }
+}
+
+
