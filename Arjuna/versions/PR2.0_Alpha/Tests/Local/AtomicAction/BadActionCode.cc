1e4bc581e5a3a9d41496770ff7a93861dc880404
Mark Little <mlittle@redhat.com>
2016-07-15 21:21:01 +0100

    Bad action test
    https://github.com/nmcl/sandbox/issues/8

---
+++ Arjuna/versions/PR2.0_Alpha/Tests/Local/AtomicAction/BadActionCode.cc
@@ -0,0 +1,67 @@
+/*
+ * Copyright 1990, 1991, 1992
+ * Computing Laboratory, University of Newcastle upon Tyne, UK.
+ *
+ * Permission to use, copy, modify and distribute this program for
+ * evaluation, teaching and/or research purposes only and without fee is
+ * hereby granted, providing that this copyright and permission notice
+ * appear on all copies and supporting documentation, and that similar
+ * conditions are imposed on any individual or organization to whom the
+ * program is distributed. The University of Newcastle upon Tyne makes no
+ * representation about the suitability of this software for any purpose.
+ * It is provided "as is" without express or implied warranty.
+ *
+ * $Id: BadActionCode.cc,v 1.2 1992/05/28 17:50:45 nsmw Exp $
+ */
+
+#include <System/iostream.h>
+
+#include <Arjuna/AtomicA.h>
+
+main(int argc, char** argv)
+{
+    AtomicAction A, B;
+
+    A.Begin();
+    cout << "A.Begin : Started\n";
+
+    B.Begin();
+    cout << "B.Begin : Started\n";
+
+    if (AtomicAction::Current() == &B)
+	cout << "Current action is B\n";
+    else
+	if (AtomicAction::Current() == &A)
+	    cout << "Current action is A\n";
+
+    cout << "Attempting to commit action A\n";
+    if (A.End() != COMMITTED)
+    {
+	cerr << "A.End() : Not Done\n";
+
+	if (AtomicAction::Current() == &B)
+	    cout << "Current action is B\n";
+	else
+	    if (AtomicAction::Current() == &A)
+		cout << "Current action is A\n";
+
+	if (B.End() != COMMITTED)
+	    cerr << "B.End() : Not Done\n";
+	else
+	    cout << "B.End() : Done\n";
+
+	if (A.End() != COMMITTED)
+	    cerr << "A.End() : Not Done\n";
+	else
+	    cout << "A.End() : Done\n";
+    }
+    else
+    {
+	cerr << "A.End() : Done\n";
+
+	if (B.End() != COMMITTED)
+	    cerr << "B.End() : Not Done\n";
+	else
+	    cout << "B.End() : Done\n";
+    }	
+}
