ca03e06e128d6b205f794796ae7a8cfe5e86e38a
Mark Little <mlittle@redhat.com>
2016-07-20 07:42:55 +0100

    Client and server Rajdoot source.
    https://github.com/nmcl/sandbox/issues/8

---
+++ Arjuna/versions/PR2.0_Alpha/RPC/Rajdoot/interface/Basic/SvRaj_.cc
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ */
+
+/*
+ * $Id: SvRaj_.cc,v 1.2 1993/03/18 15:33:32 nmcl Exp $
+ */
+
+static const char RCSid[] = "$Id: SvRaj_.cc,v 1.2 1993/03/18 15:33:32 nmcl Exp $";
+
+#ifndef STDLIB_H_
+#  include <System/stdlib.h>
+#endif
+
+#ifndef RAJDOOT_SVRAJ_H_
+#  include <RPC/Rajdoot/SvRaj.h>
+#endif
+
+char* ServerRajdoot_rpc::ServerName = NULL;
