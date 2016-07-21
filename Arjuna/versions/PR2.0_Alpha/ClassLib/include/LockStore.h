c08e41351eacfd70fcfef69defc14d33720dfb74
Mark Little <mlittle@redhat.com>
2016-07-21 07:22:39 +0100

    Lock store.
    https://github.com/nmcl/sandbox/issues/8

---
+++ Arjuna/versions/PR2.0_Alpha/ClassLib/include/LockStore.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: LockStore.h,v 1.9 1993/03/22 09:16:09 ngdp Exp $
+ */
+
+#ifndef LOCKSTORE_H_
+#define LOCKSTORE_H_
+
+/*
+ *
+ * Simple Shared Lock Store. Provides similar interface as the object store
+ * but uses shared memory. Currently very simple.
+ *
+ */
+
+#ifndef SYS_TYPES_H_
+#  include <System/sys/types.h>
+#endif
+
+#ifndef COMMONT_H_
+#  include <Common/CommonT.h>
+#endif
+
+#ifndef SHAREDSEG_H_
+#  include "SharedSeg.h"
+#endif
+
+class ObjectState;
+class LockSegment;
+class Uid;
+
+static const int SEGMENT_LIMIT = 6;		/* max number of segments per type */
+
+class LockStore : public SharedSegment
+{
+public:
+    /* Constructors and destructor */
+    
+    LockStore (const key_t key);
+    ~LockStore ();
+    
+    /* non-virtual member functions */
+    
+    Boolean read_state (const Uid& u, const TypeName tName, ObjectState*&);
+    Boolean remove_state (const Uid& u, const TypeName tName);
+    Boolean write_committed (const Uid& u, const TypeName tName, const ObjectState& state);
+    
+private:
+    long allocateSegment (size_t slotSize);
+    LockSegment *attachSegment (long segIndex);
+    long lookfor (const Uid&) const;
+    long insert (const Uid&);
+    
+    LockSegment *lockSegs[SEGMENT_LIMIT];
+    key_t lockSegKey;
+    struct MappingTable *mapTablePtr;
+};
+
+#endif
