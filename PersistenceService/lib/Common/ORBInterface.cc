2ee2aadc471b423ce22022d3470a373913f662d2
nmcl <mlittle@redhat.com>
2016-07-26 19:19:01 +0100

    Common source.
    https://github.com/nmcl/sandbox/issues/41

---
+++ PersistenceService/lib/Common/ORBInterface.cc
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 1997,
+ *
+ * Department of Computing Science,
+ * University of Newcastle upon Tyne,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id$
+ */
+
+#ifndef COMMON_ORBINTERFACE_H_
+#  include <Common/ORBInterface.h>
+#endif
+
+CORBA::ORB_ptr ORBInterface::_orb = CORBA_ORB_(_nil)();
+CORBA::BOA_ptr ORBInterface::_boa = CORBA_BOA_(_nil)();
+
+ORBInterface::ORBInterface ()
+{
+}
+
+ORBInterface::~ORBInterface ()
+{
+}
+
+CORBA::ORB_ptr ORBInterface::initORB (int& argc, char** argv, ORBid id, CORBA_(Environment& env))
+{
+    if (CORBA_(is_nil)(_orb))
+	_orb = CORBA::ORB_init(argc, argv, id, env);
+
+    CORBA_ORB_(_duplicate)(_orb);
+
+    return _orb;
+}
+
+CORBA::BOA_ptr ORBInterface::initBOA (int& argc, char** argv, OAid id, CORBA_(Environment& env))
+{
+    if (CORBA_(is_nil)(_orb))
+	return _boa;  // must be nil by definition
+
+    if (CORBA_(is_nil)(_boa))
+	_boa = _orb->BOA_init(argc, argv, id, env);
+
+    CORBA_BOA_(_duplicate)(_boa);
+
+    return _boa;
+}
+
+CORBA::ORB_ptr ORBInterface::orb ()
+{
+    return CORBA_ORB_(_duplicate)(_orb);
+}
+
+CORBA::BOA_ptr ORBInterface::boa ()
+{
+    return CORBA_BOA_(_duplicate)(_boa);
+}
---
+++ PersistenceService/lib/Common/Utility.cc
@@ -0,0 +1,153 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: Utility.cc,v 
+ */
+
+#ifndef SYS_TYPES_H_
+#   include <os/sys/types.h>
+#endif
+
+#ifndef WIN32
+#  ifndef SYS_TIME_H_
+#    include <os/sys/time.h>
+#  endif
+#else
+
+#include <winsock.h>
+
+#  ifndef TIME_H_
+#    include <os/time.h>
+#  endif
+#endif
+
+#ifndef SYS_SELECT_H_
+#  include <os/sys/select.h>
+#endif
+
+#ifndef WIN32
+#  ifndef SYS_IPC_H_
+#    include <os/sys/ipc.h>
+#  endif
+#endif
+
+#ifndef STRING_H_
+#   include <os/string.h>
+#endif
+
+#ifndef STRSTREAM_H_
+#  include <os/strstream.h>
+#endif
+
+#ifndef UTILITY_H_
+#  include <Common/Utility.h>
+#endif
+
+unsigned long checkSum ( const void *buffer, size_t len )
+{
+    unsigned long g, h = 0;
+    const char *p = (const char *)buffer;
+
+    if (buffer)
+    {
+	while (len-- > 0)
+	{
+	    h = (h << 4) + (*p);
+	    if (g = h & 0xf0000000)
+	    {
+		h = h ^ (g >> 24);
+		h = h ^ g;
+	    }
+	    p++;
+	}
+    }
+    
+    return h;
+}
+
+char* clone ( const char * const string )
+{
+    char* res;
+
+    if (string == 0)
+	res = 0;
+    else
+    {
+	res  = new char[strlen(string) + 1];
+	strcpy(res, string);
+    }
+
+    return res;
+}
+
+
+/*
+ *   The OS uses key_t values to identify its shared memory regions
+ *   and semaphores. So need to convert the object's type to a key_t.
+ *   For simplicity we hash the type into a signed quantity.
+ *   Make sure we do not generate IPC_PRIVATE by mistake
+ */
+
+#ifndef WIN32
+key_t typeToKey ( const TypeName tname )
+{
+#ifdef DEBUG
+    debug_stream << FUNCTIONS << FAC_CONCURRENCY_CONTROL << VIS_PRIVATE;
+    debug_stream << "type_toKey(\"" << tname << "\")" << endl;
+#endif
+
+    char buffer[1024];
+    ostrstream os(buffer, 1024, ios::out);
+    unsigned long h = 0;
+
+    /* Convert uid to string */
+
+    os << tname << ends;
+
+    h = checkSum(buffer, ::strlen(buffer));
+
+    if (h == IPC_PRIVATE)
+	h++;
+    
+    return (key_t) h;
+}
+#endif
+ 
+#if defined(hpux) || defined (SYSV) || defined(__ultrix) || defined(WIN32)
+int usleep ( unsigned int useconds )
+{
+  struct timeval delay;
+
+  delay.tv_sec = useconds/1000000;
+  delay.tv_usec = useconds%1000000;
+
+  (void) select (0, 0, 0, 0, &delay);
+
+  return 0;
+}
+#endif
+
+#ifdef WIN32
+
+WinInit::WinInit ()
+{
+    WORD wVersionRequested = MAKEWORD(1, 1);
+    LPWSADATA wsaData;
+
+    if (WSAStartup(wVersionRequested, wsaData) != 0)
+		error_stream << FATAL << "WINSOCK startup failed!" << endl;
+}
+
+WinInit::~WinInit ()
+{
+    (void) WSACleanup();
+}
+
+const WinInit _win;
+
+#endif
