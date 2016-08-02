f06c8446226d6010fbce56596c9d3ff089ec7551
nmcl <mlittle@redhat.com>
2016-08-02 14:58:08 +0100

    Build configuration files.

---
+++ Arjuna/versions/PR3.3_95_09_18/Include/Config/MachineRules.h
@@ -0,0 +1,22 @@
+
+Makefile :: TOPDIR_VAR/Include/Config/m_unknown.cf
+	@echo
+	@echo ***
+	@echo *** WARNING : CURDIR_VAR/Makefile may be out of date,
+	@echo ***         : older than TOPDIR_VAR/Include/Config/m_unknown.cf
+	@echo ***
+
+Makefile :: TOPDIR_VAR/Include/Config/s_linux_2_0.cf
+	@echo
+	@echo ***
+	@echo *** WARNING : CURDIR_VAR/Makefile may be out of date,
+	@echo ***         : older than TOPDIR_VAR/Include/Config/s_linux_2_0.cf
+	@echo ***
+
+Makefile :: TOPDIR_VAR/Include/Config/c_gcc_2_7.cf
+	@echo
+	@echo ***
+	@echo *** WARNING : CURDIR_VAR/Makefile may be out of date,
+	@echo ***         : older than TOPDIR_VAR/Include/Config/c_gcc_2_7.cf
+	@echo ***
+
