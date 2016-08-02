3cfc53af47bf780a77c6d616734517c4a50b7c10
nmcl <mlittle@redhat.com>
2016-08-02 15:04:51 +0100

    More System headers.

---
+++ Arjuna/versions/PR3.3_95_09_18/Include/System/wchar.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: wchar.h,v 1.1 1996/04/15 14:18:03 nmcl Exp $
+ */
+
+#ifndef WCHAR_H_
+#define WCHAR_H_
+
+#ifdef STUB
+#  pragma @NoRename
+#endif
+
+#include <wchar.h>
+
+#endif
