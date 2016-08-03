9590b49576543d1d17c6fc29720a0be77290acad
nmcl <mlittle@redhat.com>
2016-08-03 20:37:13 +0100

    stubgen

---
+++ Arjuna/versions/PR3.3_95_09_18/StubGen/include/VecT.h
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: VecT.h,v 1.4 1995/06/23 08:44:03 ngdp Exp $
+ */
+
+#ifndef VECT_H_
+#define VECT_H_
+
+/*
+ *
+ * Stub generator vector type class definition.
+ *
+ */
+
+#ifndef STCOMMONT_H_
+#  include "StCommonT.h"
+#endif
+
+#ifndef STRING_H_
+#  include "String.h"
+#endif
+
+#ifndef TDESC_H_
+#  include "TDesc.h"
+#endif
+
+#ifndef DECLSTYLE_H_
+#  include "DeclStyle.h"
+#endif
+
+class Expression;
+class ostream;
+
+class VecType : public TypeDescriptor
+{
+public:
+    VecType (Expression *, TypeDescriptor *);
+    virtual ~VecType ();
+
+    virtual Boolean attachBaseType (TypeDescriptor *);
+    virtual Boolean addStubModifier (StubModifier);
+    
+    virtual TypeDescriptor *combineTypes (TypeDescriptor *);
+    virtual TypeDescriptor *deref ();
+    virtual Boolean freezeType ();
+
+    virtual String postName (DeclStyle) const;
+    virtual String preName (DeclStyle = NORMAL_DECL, 
+			    Boolean = FALSE, Boolean = FALSE) const;
+
+    virtual Boolean isFundamental () const;
+    virtual String signature () const;
+
+    virtual StubModifier defaultMarshall () const;
+    virtual StubModifier defaultTransfer () const;
+    virtual Boolean isTransferable () const;
+
+    virtual Boolean produceMarshallingCode (ostream&, DeclStyle, Direction,
+					    const String&, const String&);
+private:
+    String indexName ();		/* generate new index var name */
+    virtual Boolean produceVecMarshallingCode (ostream&, DeclStyle, Direction,
+					       const String&, const String&);
+
+    Expression *vectorSize;		/* basic size of vector */
+    TypeDescriptor *vectorOf;		/* is a vector of this type */
+};
+
+#endif
---
+++ Arjuna/versions/PR3.3_95_09_18/StubGen/src/CvQuals.cc
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: CvQuals.cc,v 1.2 1996/04/15 14:50:31 nmcl Exp $
+ */
+
+/*
+ * Stub generator utility routines for CvQualifier variables
+ *
+ */
+
+#ifndef WIN32
+#  include <strstream.h>
+#else
+#  include <strstrea.h>
+#endif
+
+#ifndef CVQUALS_H_
+#  include "CvQuals.h"
+#endif
+
+static const char RCSid[] = "$Id: CvQuals.cc,v 1.2 1996/04/15 14:50:31 nmcl Exp $";
+
+ostream& operator<< ( ostream& s, CvQualifier cvq )
+{
+    if (cvq == QUAL_CONST)
+	s << "const ";
+    else
+	s << "volatile ";
+
+    return s;
+}
