6cedde39e1fff61300ca82f1835dc1c0acd9586e
nmcl <mlittle@redhat.com>
2016-08-03 20:36:02 +0100

    stubgen src and headers.

---
+++ Arjuna/versions/PR3.3_95_09_18/StubGen/include/Location.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: Location.h,v 1.1 1993/11/03 14:45:35 nmcl Exp $
+ */
+
+#ifndef LOCATION_H_
+#define LOCATION_H_
+
+/*
+ *
+ * Stub generator file location tracking. Used in error reporting
+ * and parsing to note locations in files of declarations
+ *
+ */
+
+#ifndef STCOMMONT_H_
+#  include "StCommonT.h"
+#endif
+
+#ifndef STRING_H_
+#  include "String.h"
+#endif
+
+class ostream;
+
+class Location
+{
+public:
+    Location ();
+    Location (const Location&);
+    Location (const String&, long);
+
+    /* public operations */
+
+    Location& operator= (const Location&);
+
+    void incrLine ();
+    void setFileName (const String&);
+    void setLineno (long);
+
+    String getFileName () const;
+    long getLineno () const;
+
+    void printLoc (ostream&) const;
+
+private:
+    /* private state */
+
+    String file;
+    long lineno;
+
+};
+
+#include "Location.n"
+
+#endif
