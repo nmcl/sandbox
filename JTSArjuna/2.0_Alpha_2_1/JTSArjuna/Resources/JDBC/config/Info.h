a47b76249c9af008e72a5bc9a53adee290458a06
Mark Little <mlittle@redhat.com>
2016-12-31 12:02:34 +0000

    https://github.com/nmcl/sandbox/issues/33

---
+++ JTSArjuna/2.0_Alpha_2_1/JTSArjuna/Resources/JDBC/config/Info.h
@@ -0,0 +1,6 @@
+#define MODULE "JDBC"
+#define SOURCEID "unknown"
+#define BUILDINFO "Arjuna Solutions Ltd. [unknown]"
+#define VERSION "4.0.0"
+#define DATE "Tue Aug 1 17:12:04 BST 2000"
+#define NOTES "none"
