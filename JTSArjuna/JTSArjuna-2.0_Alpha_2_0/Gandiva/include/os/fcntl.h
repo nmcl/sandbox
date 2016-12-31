30990bffe9420883050b00acdb0bfe2f0ce7d78e
Mark Little <mlittle@redhat.com>
2016-12-31 12:16:17 +0000

    https://github.com/nmcl/sandbox/issues/33

---
+++ JTSArjuna/JTSArjuna-2.0_Alpha_2_0/Gandiva/include/os/fcntl.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 1994, 1995, 1996,
+ *
+ * Department of Computing Science,
+ * University of Newcastle upon Tyne,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: fcntl.h,v 1.3 1996/01/02 13:13:19 nsmw Exp $
+ */
+
+#ifndef OS_FCNTL_H_
+#define OS_FCNTL_H_
+
+#include <fcntl.h>
+
+#endif
