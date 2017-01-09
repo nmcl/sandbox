6e3970fb01fd9f2f7380138c6bcf7110c056ae85
Mark Little <mlittle@redhat.com>
2017-01-09 16:52:08 +0000

    JTSArjuna 2.0 Alpha code. ORB stuff.

---
+++ JTSArjuna/JTSArjuna-2.0_Alpha_2_0/JavaCosServices/JavaCosTransactions/classes/com/arjuna/JavaCosTransactions/Filters/ObjectWrappers/vbroker/OTS_ClientActionControlWrapper.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 1996, 1997, 1998, 1999,
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: OTS_ClientActionControlWrapper.java,v 1.1 1999/09/10 15:04:04 nmcl Exp $
+ */
+
+package com.arjuna.JavaCosTransactions.Filters.ObjectWrappers.vbroker;
+
+import org.omg.CosTransactions.*;
+import com.arjuna.ArjunaOTS.*;
+import com.arjuna.JavaCosTransactions.OTS;
+import com.arjuna.JavaCosTransactions.OTS_Current;
+
+public class OTS_ClientActionControlWrapper extends ActionControlObjectWrapper
+{
+
+public Control getParentControl () throws Unavailable, NotSubtransaction, SystemException
+    {
+	Control theControl = OTS.get_current().suspend();
+	Control c = super.getParentControl();
+
+	OTS.get_current().resume(theControl);
+
+	return c;
+    }
+
+public void destroy () throws ActiveTransaction, ActiveThreads, BadControl, Destroyed, SystemException
+    {
+	Control theControl = OTS.get_current().suspend();
+	
+	super.destroy();
+
+	OTS.get_current().resume(theControl);
+    }    
+    
+public Terminator get_terminator () throws Unavailable
+    {
+	Control theControl = OTS.get_current().suspend();
+	Terminator theTerminator = super.get_terminator();
+
+	OTS.get_current().resume(theControl);
+
+	return theTerminator;
+    }
+    
+public Coordinator get_coordinator () throws Unavailable
+    {
+	Control theControl = OTS.get_current().suspend();
+	Coordinator theCoordinator = super.get_coordinator();
+
+	OTS.get_current().resume(theControl);
+
+	return theCoordinator;
+    }
+
+};
---
+++ JTSArjuna/JTSArjuna-2.0_Alpha_2_0/JavaCosServices/JavaCosTransactions/classes/com/arjuna/JavaCosTransactions/Filters/ObjectWrappers/vbroker/OTS_ClientArjunaSubtranAwareResourceWrapper.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 1996, 1997, 1998, 1999,
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: OTS_ClientArjunaSubtranAwareResourceWrapper.java,v 1.1 1999/09/10 15:04:05 nmcl Exp $
+ */
+
+package com.arjuna.JavaCosTransactions.Filters.ObjectWrappers.vbroker;
+
+import org.omg.CosTransactions.*;
+import com.arjuna.ArjunaOTS.*;
+import com.arjuna.JavaCosTransactions.OTS;
+import com.arjuna.JavaCosTransactions.OTS_Current;
+
+public class OTS_ClientArjunaSubtranAwareResourceWrapper extends ArjunaSubtranAwareResourceObjectWrapper
+{
+
+public Vote prepare_subtransaction () throws SystemException
+    {
+	Control theControl = OTS.get_current().suspend();
+	Vote v = super.prepare_subtransaction();
+
+	OTS.get_current().resume(theControl);
+
+	return v;
+    }
+    
+public void commit_subtransaction (Coordinator parent) throws SystemException
+    {
+	Control theControl = OTS.get_current().suspend();
+
+	super.commit_subtransaction(parent);
+
+	OTS.get_current().resume(theControl);
+    }
+
+public void rollback_subtransaction () throws SystemException
+    {
+	Control theControl = OTS.get_current().suspend();
+
+	super.rollback_subtransaction();
+
+	OTS.get_current().resume(theControl);
+    }    
+
+public Vote prepare () throws HeuristicMixed, HeuristicHazard, SystemException
+    {
+	Control theControl = OTS.get_current().suspend();
+	Vote v = super.prepare();
+
+	OTS.get_current().resume(theControl);
+
+	return v;
+    }
+
+public void rollback () throws HeuristicCommit, HeuristicMixed, HeuristicHazard, SystemException
+    {
+	Control theControl = OTS.get_current().suspend();
+
+	super.rollback();
+
+	OTS.get_current().resume(theControl);
+    }    
+
+public void commit () throws NotPrepared, HeuristicRollback, HeuristicMixed, HeuristicHazard, SystemException
+    {
+	Control theControl = OTS.get_current().suspend();
+
+	super.commit();
+
+	OTS.get_current().resume(theControl);
+    }    
+
+public void commit_one_phase () throws HeuristicHazard, SystemException
+    {
+	Control theControl = OTS.get_current().suspend();
+
+	super.commit_one_phase();
+
+	OTS.get_current().resume(theControl);
+    }
+
+public void forget () throws SystemException
+    {
+	Control theControl = OTS.get_current().suspend();
+
+	super.forget();
+
+	OTS.get_current().resume(theControl);
+    }    
+
+};
