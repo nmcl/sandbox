402ba7ae17744950b4418158ee6dd004d3019e99
Mark Little <mlittle@redhat.com>
2017-01-15 15:50:39 +0000

    https://github.com/nmcl/sandbox/issues/33

---
+++ JTSArjuna/JTSArjuna-2.0_Alpha_2_0/JavaCosServices/JavaCosTransactions/tests/Arjuna/intertest.bat
@@ -0,0 +1,16 @@
+@echo off
+
+if "%JTSARJUNA_HOME%"=="" goto error
+
+<<<<<<< intertest.bat
+java -DORBservices=com.arjuna.JavaCosTransactions.Filters.Interposition.vbroker -DOBJECTSTORE_DIR=%JTSARJUNA_HOME%\ObjectStore ImplicitArjunaClient
+=======
+java -DORBservices=com.arjuna.JavaCosTransactions.Filters.Interposition.vbroker -DOBJECTSTORE_DIR=%JTSARJUNA_HOME%\ObjectStore %1 %2 %3 %4 %5
+>>>>>>> 1.5
+goto end
+
+:error
+
+echo Environment variable JTSARJUNA_HOME is not set.
+
+:end
