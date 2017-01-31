cf7279b887117b2981093453a5cb89579a939bae
Mark Little <mlittle@redhat.com>
2017-01-31 16:56:37 +0000

    Transactional JDBC.

---
+++ JTSArjuna/JTSArjuna-2.0_Alpha_2_0/Resources/JDBC/classes/com/arjuna/JDBC/JDBCType.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 1998, 1999,
+ *
+ * Department of Computing Science,
+ * University of Newcastle upon Tyne,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: JDBCType.java,v 1.1 1999/06/22 15:12:37 nmcl Exp $
+ */
+
+package com.arjuna.JDBC;
+
+public class JDBCType
+{
+
+public static final String typeProperty = "JDBC_TYPE";
+    
+public static final int ONE_POINT_ZERO = 0;
+public static final int TWO_POINT_ZERO = 1;
+    
+};
