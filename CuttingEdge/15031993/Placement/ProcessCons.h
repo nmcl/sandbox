311051b1645b33554dc8ff8058fdbff2d5b1b8b7
Mark Little <mlittle@redhat.com>
2017-07-22 20:15:57 +0100

    Slightly updated version of placement
    https://github.com/nmcl/sandbox/issues/59

---
+++ CuttingEdge/15031993/Placement/ProcessCons.h
@@ -0,0 +1,20 @@
+#ifndef ProcessCons_
+#define ProcessCons_
+
+#include "common.h"
+#include "Process.h"
+
+class ProcessCons
+{
+public:
+    ProcessCons (Process &p, ProcessCons *n);
+    Process     *car ();
+    ProcessCons *cdr ();
+    void SetfCdr (ProcessCons *n);
+
+private:
+    Process *Proc;
+    ProcessCons *Next;
+};
+
+#endif
