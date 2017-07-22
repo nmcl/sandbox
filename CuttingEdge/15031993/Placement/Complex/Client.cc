89994cdfe01fd4b04a64b9193939695075d684fe
Mark Little <mlittle@redhat.com>
2017-07-22 20:13:23 +0100

    A slightly updated version of replica placement
    https://github.com/nmcl/sandbox/issues/59

---
+++ CuttingEdge/15031993/Placement/Complex/Client.cc
@@ -0,0 +1,140 @@
+#include <iostream.h>
+
+#include "Client.h"
+
+extern Object* obj;
+extern double NumberOfOperations;
+extern double OperationsPerformed;
+
+Client::Client (long id, double op_rate)
+{
+    rate = op_rate;
+    ClientID = id;
+    ArrivalTime = new ExponentialStream(rate);
+    ProcessTime = new ExponentialStream(rate*0.05);
+    WaitTime = new UniformStream(1, 5);
+    OperationType = new Draw(0.2); // chance that operation is write
+}
+
+Client::~Client ()
+{
+    delete ArrivalTime;
+    delete ProcessTime;
+}
+
+double Client::GetRate () { return rate; }
+
+void Client::SetRate (double n_value)
+{
+    rate = n_value;
+    
+    if (ArrivalTime)
+    {
+	delete ArrivalTime;
+	ArrivalTime = new ExponentialStream(rate);
+    }
+    
+    if (ProcessTime)
+    {
+	delete ProcessTime;
+	ProcessTime = new ExponentialStream(rate*0.05);
+    }
+}
+
+void Client::MakeCall (lock_code locktype)
+{
+    boolean done = false;
+
+    while (!done)
+    {
+	if (locktype != unlocked)
+	    NumberOfOperations++;
+	if (!obj->PerformOperation(locktype, this))
+	{
+#ifdef DEBUG
+	    cout << "Cannot perform operation\n" << flush;
+#endif
+	    Hold((*WaitTime)());
+	}
+	else
+	{
+	    done = true;
+	    if (locktype != unlocked)
+		OperationsPerformed++;
+	}
+    }
+}
+
+void Client::Body ()
+{
+    double x, holdtime;
+    lock_code lockas;
+    boolean finished;
+
+    for (;;)
+    {
+	Hold((*ArrivalTime)());
+
+	holdtime = (*ProcessTime)();
+
+#ifdef DEBUG
+	cout << "Client " << ClientID << " holding for " << holdtime << "\n" << flush;
+#endif
+
+	x = holdtime + CurrentTime();
+
+#ifdef RANDOP
+	if ((*OperationType)())
+	{
+#ifdef DEBUG
+	    cout << "Client " << ClientID << " making read operation\n" << flush;
+#endif
+	    lockas = read;
+	}
+	else
+	{
+#ifdef DEBUG
+	    cout << "Client " << ClientID << " making write operation\n" << flush;
+#endif
+	    lockas = write;
+	}
+#else
+	lockas = write;
+//	lockas = read;
+#endif
+	
+	MakeCall(lockas);
+	finished = false;
+	while (!finished)
+	{
+	    Hold(holdtime);
+
+#ifdef DEBUG
+	    cout << "Client " << ClientID << " woken up\n" << flush;
+#endif
+
+	    if (x != CurrentTime())
+	    {
+		if (!obj->CheckQuorum(lockas))
+		{
+#ifdef DEBUG
+		    cout << "Client " << ClientID << " aborting\n" << flush;
+#endif
+		    MakeCall(unlocked);
+		    finished = true;
+		}
+		else
+		    holdtime = x - CurrentTime();
+	    }
+	    else
+	    {
+#ifdef DEBUG
+		cout << "Client " << ClientID << " successfully completed call\n" << flush;
+#endif
+
+		MakeCall(unlocked);
+		finished = true;
+	    }
+	}
+    }
+}
