bbd5f2422f55aba291e44fe0d4103144a1417b2e
Mark Little <mlittle@redhat.com>
2017-07-23 10:01:59 +0100

    ReplicaPlacement work.

---
+++ CuttingEdge/08071997/ReplicaPlacement/Basic/AC.cc
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ */
+
+#include <iostream.h>
+
+#ifndef AC_H_
+#include "AC.h"
+#endif
+
+
+AC::AC (long RQ, long WQ, double RT) : Protocol(RQ, WQ, RT)
+{
+    name = "AvailableCopies";
+}
+
+AC::~AC () {}
+
+void AC::SetWriteQuorum (long  WQ)
+{
+    WriteQuorum = WQ;
+    SetReadQuorum(1);
+
+    cout << "Write Quorum is " << WriteQuorum << "\n" << flush;
+    cout << "Read Quorum is " << ReadQuorum << "\n" << flush;
+}
+
+void AC::SetReadQuorum (long RQ) { ReadQuorum = RQ; }
+
+long AC::GetMinimum () { return 1; }
