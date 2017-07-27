794c29b5e34d57b9f96a285b56faaf4e776ff928
Mark Little <mlittle@redhat.com>
2017-07-27 19:23:01 +0100

    thread wrapper

---
+++ CuttingEdge/25091992/Placement/thread.cc
@@ -0,0 +1,50 @@
+#include "thread.h"
+
+Thread::Thread ()
+{
+    Thread *marker = head;
+
+    if (head != 0)
+    {
+	while (marker->next != 0)
+	    marker = marker->next;
+
+	marker->next = this;
+	marker->next->prev = marker;
+	marker = marker->next;
+	marker->next = 0;
+    }
+    else
+    {
+	this->prev = 0;
+	this->next = 0;
+	head = this;
+    }
+
+    thread_key = 0;
+    // Actual thread key value MUST be set in derived class constructor
+}
+
+Thread::~Thread ()
+{
+    if (this->prev != 0)
+	this->prev->next = this->next;
+    else
+	head = this->next;
+}
+
+long Thread::Identity () { return thread_key; }
+
+Thread *Thread::Self ()
+{
+    if (!head) return 0;
+
+    // Use any thread object (e.g., head) to get access to the current thread
+    long my_id = head->Current_Thread();
+
+    for (Thread *marker = head; marker; marker=marker->next)
+	if (marker->thread_key == my_id)
+	    break;
+
+    return marker;
+}
