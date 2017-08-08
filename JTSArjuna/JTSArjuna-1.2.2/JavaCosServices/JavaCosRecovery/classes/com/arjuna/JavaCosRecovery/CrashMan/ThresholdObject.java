2fd377570af04cfb23656b70a7b98cd64c0cd825
Mark Little <m.c.little@ncl.ac.uk>
2017-08-08 12:00:38 -0400

    More recovery service

---
+++ JTSArjuna/JTSArjuna-1.2.2/JavaCosServices/JavaCosRecovery/classes/com/arjuna/JavaCosRecovery/CrashMan/ThresholdObject.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 1996, 1997, 1998,
+ *
+ * Department of Computing Science,
+ * University of Newcastle upon Tyne,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: ThresholdObject.java,v 1.2 1998/07/06 10:49:53 nmcl Exp $
+ */
+
+package com.arjuna.JavaCosRecovery.CrashMan;
+
+public interface ThresholdObject
+{
+
+public boolean reached ();
+
+};
