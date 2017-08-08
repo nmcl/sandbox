65bd06c245eec330615a3b6bd0e8fe38c26b4bfd
Mark Little <m.c.little@ncl.ac.uk>
2017-08-08 11:58:37 -0400

    Some performance tests

---
+++ JTSArjuna/JTSArjuna-1.2.2/JavaArjunaLite/tests/Performance/PerformanceTest4.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 1996, 1997, 1998,
+ *
+ * Department of Computing Science,
+ * University of Newcastle upon Tyne,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: PerformanceTest4.java,v 1.1 1998/11/12 09:39:25 nmcl Exp $
+ */
+
+import com.arjuna.JavaArjunaLite.Atomic.*;
+import com.arjuna.JavaGandiva.Common.*;
+
+import java.lang.NumberFormatException;
+
+public class PerformanceTest4
+{
+    
+public static void main (String[] args)
+    {
+	long iters = 1000;
+
+	for (int i = 0; i < args.length; i++)
+	{
+	    if (args[i].compareTo("-iter") == 0)
+	    {
+		try
+		{
+		    iters = Long.parseLong(args[i+1]);
+		}
+		catch (NumberFormatException e)
+		{
+		}
+	    }
+	}
+
+	AtomicAction A = new AtomicAction();
+	A.begin();
+	A.commit();
+	A = null;
+	
+	long t1 = System.currentTimeMillis();
+	
+	for (int c = 0; c < iters; c++)
+	{
+	    A = new AtomicAction();
+	
+	    A.begin();
+	    
+	    A.commit();
+
+	    A = null;
+	}
+
+	long t2 = System.currentTimeMillis();
+
+	System.out.println(t1+" "+t2);
+	
+	System.out.print("Time taken to create/commit "+iters+" atomic actions: ");
+
+	System.out.println(t2-t1+" milliseconds");
+
+	AtomicAction B = null;
+	t1 = System.currentTimeMillis();
+
+	for (int c = 0; c < iters; c++)
+	{
+	    A = new AtomicAction();
+	    
+	    A.begin();
+
+	    B = new AtomicAction();
+
+	    B.begin();
+
+	    B.commit();
+	    
+	    A.commit();
+
+	    B = null;
+	    A = null;
+	}
+
+	t2 = System.currentTimeMillis();
+
+	System.out.println(t1+" "+t2);
+	
+	System.out.print("Time taken to create/commit "+iters+" nested atomic actions: ");
+
+	System.out.println(t2-t1+" milliseconds.");
+    }
+    
+};
