88e9920de533de9d20c6baa9ad1d4f44c34efdbb
Mark Little <m.c.little@ncl.ac.uk>
2017-08-10 11:22:41 -0400

    interface/implementation work

---
+++ JTSArjuna/JTSArjuna-1.2.2/JavaCosServices/JavaCosTransactions/classes/com/arjuna/JavaCosTransactions/JavaCosTransactionsNames.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 1997, 1998,
+ *
+ * Department of Computing Science,
+ * University of Newcastle upon Tyne,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: JavaCosTransactionsNames.java,v 1.2 1998/07/06 13:28:34 nmcl Exp $
+ */
+
+package com.arjuna.JavaCosTransactions;
+
+import com.arjuna.JavaGandiva.Common.ClassName;
+
+public class JavaCosTransactionsNames
+{
+
+public static ClassName AbstractRecord_OTS_ResourceRecord ()
+    {
+	return new ClassName("OTS_ResourceRecord");
+    }
+    
+public static ClassName AbstractRecord_OTS_AbstractRecord ()
+    {
+        return new ClassName("OTS_AbstractRecord");
+    }
+
+};
