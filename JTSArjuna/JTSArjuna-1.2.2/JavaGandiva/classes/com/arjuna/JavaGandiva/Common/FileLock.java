7c9e1b5829b69e62f41996d869e56a83e0ac1a1a
Mark Little <m.c.little@ncl.ac.uk>
2017-08-11 12:16:39 -0400

    file lock and state pickling

---
+++ JTSArjuna/JTSArjuna-1.2.2/JavaGandiva/classes/com/arjuna/JavaGandiva/Common/FileLock.java
@@ -0,0 +1,312 @@
+/*
+ * Copyright (C) 1996, 1997,
+ *
+ * Department of Computing Science,
+ * University of Newcastle upon Tyne,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: FileLock.java,v 1.2 1998/07/06 13:30:45 nmcl Exp $
+ */
+
+package com.arjuna.JavaGandiva.Common;
+
+import java.io.*;
+
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.lang.InterruptedException;
+
+/*
+ * Since there is no native Java way of locking a file, we
+ * have to implement our own. Unfortunately, it appears as though
+ * we can only assume that rename is atomic. We base the locking on
+ * this then: rename the lock file and update it with the lock owners.
+ *
+ * How it works:
+ *
+ * for every file we want to lock we create an _lock file. This file
+ * contains information about who is locking the file, and in what mode.
+ * (Single writer, multiple readers.) To guarantee atomicity of update, we
+ * move (rename) the actual file each time we want to lock it and update the
+ * lock file. When this is done, we move (rename) it back. Almost like a
+ * two-phase commit protocol! Currently we don't support re-entrant locking.
+ */
+
+public class FileLock
+{
+
+public static final int F_RDLCK = 0;
+public static final int F_WRLCK = 1;
+
+public static final int defaultTimeout = 100;  // milliseconds
+public static final int defaultRetry = 10;
+     
+public FileLock (File name)
+    {
+	_theFile = name;
+	_lockFile = new File(new String(name.toString()+"_lock"));
+	_shadowFile = new File(new String(name.toString()+"#"));
+	_lockFileShadow = new File(new String(name.toString()+"_lock#"));
+	_timeout = FileLock.defaultTimeout;
+	_retry = FileLock.defaultRetry;
+    }
+
+public FileLock (File name, long timeout, long retry)
+    {
+	_theFile = name;	
+	_lockFile = new File(new String(name.toString()+"_lock"));
+	_shadowFile = new File(new String(name.toString()+"#"));	
+	_lockFileShadow = new File(new String(name.toString()+"_lock#"));
+	_timeout = timeout;
+	_retry = retry;
+    }
+
+public boolean createFile ()
+    {
+	byte b[] = new byte[1];
+
+	try
+	{
+	    DataOutputStream ofile = new DataOutputStream(new FileOutputStream(_theFile));
+
+	    if (!_theFile.exists())
+	    {
+		ofile.write(b, 0, 0);  // create zero-length file
+
+		return true;
+	    }
+	    else
+		return false;
+	}
+	catch (IOException e)
+	{
+	    System.out.println("An error occurred while creating file "+_theFile);
+	    return false;
+	}	
+    }
+    
+public boolean lock (int lmode, boolean create)
+    {
+	if (create && !_theFile.exists())
+	{
+	    createFile();
+	}
+	
+	/*
+	 * If the lock file exists, and the mode is exclusive, then we can
+	 * immediately return false.
+	 *
+	 * Currently we do not implement re-entrant locking, which requires some
+	 * owner id.
+	 */
+
+	if (_lockFile.exists() && _lockFile.isFile())
+	{
+	    if (lmode == FileLock.F_WRLCK)
+		return false;
+	}
+
+	int number = 0;
+
+	if (grabFile())  // have we moved the file (if it exists)?
+	{
+	    try
+	    {
+		DataInputStream ifile = new DataInputStream(new FileInputStream(_lockFile));
+		int value = ifile.readInt();
+	    
+		/*
+		 * Already exclusively locked.
+		 */
+	    
+		if (value == FileLock.F_WRLCK)
+		{
+		    ifile.close();
+		    releaseFile();
+		    
+		    return false;
+		}
+		else
+		    number = ifile.readInt();
+
+		ifile.close();
+	    }
+	    catch (FileNotFoundException e)
+	    {
+	    }
+	    catch (IOException e)
+	    {
+		/*
+		 * Something went wrong. Abandon.
+		 */
+
+		releaseFile();
+
+		return false;
+	    }
+
+	    try
+	    {
+		DataOutputStream ofile = new DataOutputStream(new FileOutputStream(_lockFileShadow));
+
+		number++;
+	
+		ofile.writeInt(lmode);
+		ofile.writeInt(number);
+
+		ofile.close();
+
+		_lockFileShadow.renameTo(_lockFile);
+		
+		releaseFile();
+	
+		return true;
+	    }
+	    catch (IOException e)
+	    {
+		/*
+		 * Something went wrong. Abandon. Lock file is ok since we haven't touched it.
+		 */
+
+		releaseFile();
+
+		_lockFileShadow.delete();  // just in case!
+
+		return false;
+	    }
+	}
+
+	return false;
+    }
+
+public boolean unlock ()
+    {
+	if (!_lockFile.exists() && _lockFile.isFile())
+	    return false;
+
+	/*
+	 * Has the file been deleted. If so, simply remove any remaining
+	 * locking file.
+	 */
+	
+	if (!_theFile.exists())
+	{
+	    if (_lockFile.exists())
+		_lockFile.delete();
+
+	    return true;
+	}
+	
+	if (grabFile())
+	{
+	    int number = 0, mode = 0;
+	    
+	    try
+	    {
+		DataInputStream ifile = new DataInputStream(new FileInputStream(_lockFile));
+
+		mode = ifile.readInt();
+		number = ifile.readInt();
+		ifile.close();
+
+		number--;
+
+		if (number == 0)
+		{
+		    _lockFile.delete();
+		    releaseFile();
+
+		    return true;
+		}
+	    }
+	    catch (FileNotFoundException e)
+	    {
+		releaseFile();
+
+		return false;
+	    }
+	    catch (IOException e)
+	    {
+		releaseFile();
+
+		return false;
+	    }
+	    
+	    try
+	    {
+		DataOutputStream ofile = new DataOutputStream(new FileOutputStream(_lockFileShadow));
+
+		ofile.writeInt(mode);
+		ofile.writeInt(number);
+		ofile.close();
+
+		_lockFileShadow.renameTo(_lockFile);
+
+		releaseFile();
+		
+		return true;
+	    }
+	    catch (IOException e)
+	    {
+		releaseFile();
+		
+		_lockFileShadow.delete();
+		
+		return false;
+	    }
+	}
+
+	return false;
+    }
+
+private boolean grabFile ()
+    {
+	for (int i = 0; i < _retry; i++)
+	{
+	    /*
+	     * If the shadow exists then someone else is in the
+	     * process of modifying the lock data. Wait.
+	     */
+		
+	    if (_shadowFile.exists())
+	    {
+		if (!_shadowFile.isFile())  // not a file!!
+		    return false;
+
+		try
+		{
+		    Thread.sleep(_timeout);
+		}
+		catch (InterruptedException e)
+		{
+		}
+	    }
+	    else
+	    {
+		if (_theFile.renameTo(_shadowFile))
+		{
+		    return true;
+		}
+	    }
+	}
+
+	return false;
+    }
+
+private boolean releaseFile ()
+    {
+	return _shadowFile.renameTo(_theFile);
+    }
+    
+    
+private File _theFile;
+private File _shadowFile;
+private File _lockFile;
+private File _lockFileShadow;
+private long _timeout;
+private long _retry;
+    
+};
+
+
---
+++ JTSArjuna/JTSArjuna-1.2.2/JavaGandiva/classes/com/arjuna/JavaGandiva/Common/ObjectName.java
@@ -0,0 +1,371 @@
+/*
+ * Copyright (C) 1996, 1997, 1998,
+ *
+ * Department of Computing Science,
+ * University of Newcastle upon Tyne,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: ObjectName.java,v 1.4 1998/07/20 09:08:24 nmcl Exp $
+ */
+
+package com.arjuna.JavaGandiva.Common;
+
+import com.arjuna.JavaGandiva.Interface.NameService;
+import java.io.PrintStream;
+
+import java.lang.NullPointerException;
+import java.io.IOException;
+
+public class ObjectName
+{
+    /*
+     * All numbers are signed, but we leave this in for compatibility.
+     */
+    
+public static final int SIGNED_NUMBER = 0;
+public static final int UNSIGNED_NUMBER = 1;
+public static final int STRING = 2;
+public static final int OBJECTNAME = 3;
+public static final int CLASSNAME = 4;
+public static final int UID = 5;
+
+public ObjectName (String objectName)
+    {
+	_mech = null;
+	_name = null;
+	_nameService = null;
+
+	if (objectName == null)
+	{
+	    _mech = _invalidString;
+	    _name = _invalidString;
+	}
+	else
+	{
+	    int mechLength = objectName.indexOf(nameDelim);
+
+	    if (mechLength != -1)
+	    {
+		int nameLength = objectName.length() - mechLength - nameDelim.length();
+
+		_mech = new String(objectName.substring(0, mechLength));
+
+		if (nameLength > 0)
+		    _name = objectName.substring((mechLength+nameDelim.length()), objectName.length());
+		else
+		    _name = new String("");
+	    }
+	}
+
+	initNameService();
+    }
+
+public ObjectName (ObjectName objectName)
+    {
+	_mech = null;
+	_name = null;
+	_nameService = null;
+
+	this.copy(objectName);
+    }
+
+public ObjectName (NameService nameService, String objectName)
+    {
+	_mech = null;
+	_name = null;
+	_nameService = null;
+	
+	if ((nameService != null) && (objectName != null))
+	{
+	    _nameService = nameService;
+
+	    String fullMechName = _nameService.impleClassName().stringForm();
+	    int appendedString = fullMechName.indexOf(nameServiceString);
+
+	    _mech = new String(fullMechName.substring(0, appendedString-fullMechName.length()));
+	    _name = new String(objectName);
+	}
+	else
+	{
+	    if (nameService == null)
+		System.out.println("ERROR: ObjectName - no name service given.");
+	
+	    if (objectName == null)
+		System.out.println("ERROR: ObjectName - no object name give.");
+
+	    _mech = _invalidString;
+	    _name = _invalidString;
+	}
+    }
+
+public void finalize ()
+    {
+	_mech = null;
+	_name = null;
+	_nameService = null;
+    }
+    
+public int attributeType (String attrName) throws NullPointerException, IOException
+    {
+	if (_nameService != null)
+	    return _nameService.attributeType(_name, attrName);
+	else
+	    throw(new NullPointerException("NameService implementation not set"));
+    }
+
+public String firstAttributeName () throws NullPointerException, IOException
+    {
+	if (_nameService != null)
+	    return _nameService.firstAttributeName(_name);
+	else
+	    throw(new NullPointerException("NameService implementation not set"));
+    }
+
+public String nextAttributeName (String attrName) throws NullPointerException, IOException
+    {
+	if (_nameService != null)
+	    return _nameService.nextAttributeName(_name, attrName);
+	else
+	    throw(new NullPointerException("NameService implementation not set"));
+    }
+
+public long getLongAttribute (String attrName) throws NullPointerException, IOException
+    {
+	if (_nameService != null)
+	    return _nameService.getLongAttribute(_name, attrName);
+	else
+	    throw(new NullPointerException("NameService implementation not set"));
+    }
+
+public String getStringAttribute (String attrName) throws NullPointerException, IOException
+    {
+	if (_nameService != null)
+	    return _nameService.getStringAttribute(_name, attrName);
+	else
+	    throw(new NullPointerException("NameService implementation not set"));
+    }
+
+public ObjectName getObjectNameAttribute (String attrName) throws NullPointerException, IOException
+    {
+	if (_nameService != null)
+	    return _nameService.getObjectNameAttribute(_name, attrName);
+	else
+	    throw(new NullPointerException("NameService implementation not set"));
+    }
+
+public ClassName getClassNameAttribute (String attrName) throws NullPointerException, IOException
+    {
+	if (_nameService != null)
+	    return _nameService.getClassNameAttribute(_name, attrName);
+	else
+	    throw(new NullPointerException("NameService implementation not set"));
+    }
+
+public Uid getUidAttribute (String attrName) throws NullPointerException, IOException
+    {
+	if (_nameService != null)
+	    return _nameService.getUidAttribute(_name, attrName);
+	else
+	    throw(new NullPointerException("NameService implementation not set"));
+    }
+
+public void setLongAttribute (String attrName, long value) throws NullPointerException, IOException
+    {
+	if (_nameService != null)
+	    _name = _nameService.setLongAttribute(_name, attrName, value);
+	else
+	    throw(new NullPointerException("NameService implementation not set"));
+    }
+
+public void setStringAttribute (String attrName, String value) throws NullPointerException, IOException
+    {
+	if (_nameService != null)
+	    _name = _nameService.setStringAttribute(_name, attrName, value);
+	else
+	    throw(new NullPointerException("NameService implementation not set"));	
+    }
+
+public void setObjectNameAttribute (String attrName, ObjectName value) throws NullPointerException, IOException
+    {
+	if (_nameService != null)
+	    _name = _nameService.setObjectNameAttribute(_name, attrName, value);
+	else
+	    throw(new NullPointerException("NameService implementation not set"));
+    }
+
+public void setClassNameAttribute (String attrName, ClassName value) throws NullPointerException, IOException
+    {
+	if (_nameService != null)
+	    _name = _nameService.setClassNameAttribute(_name, attrName, value);
+	else
+	    throw(new NullPointerException("NameService implementation not set"));
+    }
+
+public void setUidAttribute (String attrName, Uid value) throws NullPointerException, IOException
+    {
+	if (_nameService != null)
+	    _name = _nameService.setUidAttribute(_name, attrName, value);
+	else
+	    throw(new NullPointerException("NameService implementation not set"));
+    }
+
+public boolean removeAttribute (String attrName) throws IOException
+    {
+	if (_nameService != null)
+	{
+	    _name = _nameService.removeAttribute(_name, attrName);
+
+	    if (_name != null)
+		return true;
+	}
+	
+	return false;
+    }
+
+public String uniqueAttributeName () throws NullPointerException, IOException
+    {
+	if (_nameService != null)
+	    return _nameService.uniqueAttributeName(_name);
+	else
+	    throw(new NullPointerException("NameService implementation not set"));
+    }
+
+public static ObjectName uniqueObjectName (String mech) throws NullPointerException, IOException
+    {
+	NameService nameService = null;
+	ObjectName objectName = ObjectName.invalid();
+
+	if ((mech != null) && (mech.compareTo(_invalidString) != 0))
+	{
+	    String nameServiceMech = mech + nameServiceString;
+	    ClassName nameServiceMechClassName = new ClassName(nameServiceMech);
+	    nameService = NameService.create(nameServiceMechClassName);
+
+	    if (nameService != null)
+		objectName = nameService.uniqueObjectName();
+	    
+	    nameService = null;
+	}
+
+	return objectName;
+    }
+
+public synchronized void copy (ObjectName objectName)
+    {
+	if (objectName == this)
+	    return;
+	
+	_nameService = null;
+        _mech = null;
+        _name = null;
+
+	if (objectName._mech != null)
+	{
+	    _mech = new String(objectName._mech);
+
+	    if (objectName._nameService != null)
+		_nameService = new NameService(objectName._nameService);
+	}
+
+	if (objectName._name != null)
+	    _name = new String(objectName._name);
+
+	initNameService();
+    }
+
+public boolean equals (ObjectName objectName)
+    {
+	return (((_name == objectName._name) || 
+		((_name != null) && (objectName._name != null) && (_name.compareTo(objectName._name) == 0))) &&
+	    ((_mech == objectName._mech) ||
+	     ((_mech != null) && (objectName._mech != null) && (_mech.compareTo(objectName._mech) == 0))));
+    }
+
+public boolean notEquals (ObjectName objectName)
+    {
+	return (((_name != objectName._name) && 
+		((_name == null) || (objectName._name == null) || (_name.compareTo(objectName._name) != 0))) ||
+	    ((_mech != objectName._mech) &&
+	     ((_mech == null) || (objectName._mech == null) || (_mech.compareTo(objectName._mech) != 0))));
+    }
+
+public String getMechanismName ()
+    {
+	return _mech;
+    }
+
+public String stringForm ()
+    {
+	return new String(_mech + nameDelim + _name);
+    }
+
+public static ObjectName invalid ()
+    {
+	return _invalid;
+    }
+
+public void print (PrintStream strm)
+    {
+	strm.print("<ObjectName:"+_mech+nameDelim+_name+">");
+    }
+
+public String toString ()
+    {
+	return stringForm();
+    }
+
+public static void print (int t)
+    {
+	switch (t)
+	{
+	case ObjectName.SIGNED_NUMBER:
+	    System.out.print("SIGNED_NUMBER");
+	    break;
+	case ObjectName.UNSIGNED_NUMBER:
+	    System.out.print("UNSIGNED_NUMBER");
+	    break;
+	case ObjectName.STRING:
+	    System.out.print("STRING");
+	    break;
+	case ObjectName.OBJECTNAME:
+	    System.out.print("OBJECTNAME");
+	    break;
+	case ObjectName.CLASSNAME:
+	    System.out.print("CLASSNAME");
+	    break;
+	case ObjectName.UID:
+	    System.out.print("UID");
+	    break;
+	}
+    }
+    
+private void initNameService ()
+    {
+	if (_nameService != null)
+	    return;
+
+	if ((_mech != null) && (_mech.compareTo(_invalidString) != 0))
+	{
+	    String nameServiceMech = new String(_mech + nameServiceString);
+	    ClassName nameServiceMechClassName = new ClassName(nameServiceMech);
+	    
+	    _nameService = NameService.create(nameServiceMechClassName);
+	    
+	    nameServiceMech = null;
+	}
+	else
+	    _nameService = null;
+    }
+
+private String _mech;
+private String _name;
+private NameService _nameService;
+    
+private static final String _invalidString = "invalid";
+private static final String nameServiceString = "NameServiceImple";
+private static final String nameDelim = ":";
+private static final String nullString = "";
+private static final ObjectName _invalid = new ObjectName("invalid:invalid");
+
+}
