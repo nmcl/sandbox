3dcae904e979cf458ff7a45d0581f413d5e3c0fe
Mark Little <m.c.little@ncl.ac.uk>
2017-08-11 11:49:41 -0400

    Various types of heuristic outcome

---
+++ JTSArjuna/JTSArjuna-1.2.2/JavaCosServices/JavaCosTransactions/classes/com/arjuna/jta/JTA_HeuristicType.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 1999,
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: JTA_HeuristicType.java,v 1.1.2.1 1999/02/16 14:56:22 nmcl Exp $
+ */
+
+package com.arjuna.jta;
+
+public class JTA_HeuristicType
+{
+
+public static final int HEURISTIC_MIXED = 0;
+public static final int HEURISTIC_HAZARD = 1;
+
+};
