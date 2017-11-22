74ef5ea92e9fdadbfaf8eeae0b755c4e11c0a910
Mark Little <mlittle@redhat.com>
2017-11-22 19:29:38 +0000

    Two phase prepare outcome status.

---
+++ Arjuna/versions/PR3.3.2_97_10_01/Include/Arjuna/PrepOut.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: PrepOut.h,v 1.1 1993/11/03 14:27:58 nmcl Exp $
+ */
+
+#ifndef PREPOUT_H_
+#define PREPOUT_H_
+
+enum PrepareOutcome
+{
+    PREP_OK, PREP_NOTOK, READONLY
+};
+
+
+#endif
