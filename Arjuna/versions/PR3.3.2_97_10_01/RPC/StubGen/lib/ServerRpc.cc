a625abc14e2326f6e3d8d456de5d5a13b25ed4d9
Mark Little <mlittle@redhat.com>
2017-11-27 07:43:57 +0000

    Stub generation code for RPC.

---
+++ Arjuna/versions/PR3.3.2_97_10_01/RPC/StubGen/lib/ServerRpc.cc
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: ServerRpc.cc,v 1.1 1994/06/01 14:26:01 ngdp Exp $
+ */
+
+static const char RCSid[] = "$Id: ServerRpc.cc,v 1.1 1994/06/01 14:26:01 ngdp Exp $";
+
+#include <StubGen/ServerRpc.h>
+
+ServerRpc::ServerRpc ()
+{
+}
+
+ServerRpc::~ServerRpc ()
+{
+}
