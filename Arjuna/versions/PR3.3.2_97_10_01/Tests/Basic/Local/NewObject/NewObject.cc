37de2bf4779fb58ad6ea30571ab349ceced08eb7
Mark Little <mlittle@redhat.com>
2017-11-30 12:37:39 +0000

    local test

---
+++ Arjuna/versions/PR3.3.2_97_10_01/Tests/Basic/Local/NewObject/NewObject.cc
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 1993
+ * 
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: NewObject.cc,v 1.2 1994/09/14 08:56:21 ngdp Exp $
+ */
+
+#include <System/iostream.h>
+
+#include <Common/Boolean.h>
+#include <Arjuna/AtomicA.h>
+
+#include "ObjTest.h"
+
+main()
+{
+    int res = 0;
+    ObjTest* x;
+    AtomicAction A;
+    
+    A.Begin();
+    cout << "A.Begin : Started\n";
+
+    cout << "Creating new persistent object\n\n" << flush;
+    x = new ObjTest(res, FALSE);
+
+    cout << "\nCommitting top-level action (should call save_state)\n" << flush;
+    if (A.End() != COMMITTED)
+	cout << "A.End() : Not Done\n";
+    else
+	cout << "A.End() : Done\n";
+
+    delete x;
+    return 0;
+}
