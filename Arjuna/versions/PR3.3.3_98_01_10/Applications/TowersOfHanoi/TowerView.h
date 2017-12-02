ed76c31adab8771b692293f258b68738720a4617
Mark Little <mlittle@redhat.com>
2017-12-02 12:43:32 +0000

    Towers of Hanoi.

---
+++ Arjuna/versions/PR3.3.3_98_01_10/Applications/TowersOfHanoi/TowerView.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ */
+
+/*
+ * $Id: TowerView.h,v 1.1 1993/11/03 12:25:33 nmcl Exp $
+ */
+
+#ifndef TOWERVIEW_H_
+#define TOWERVIEW_H_
+
+class Uid;
+
+extern char* PinHost;
+extern Uid*  PinUid;
+
+#endif
