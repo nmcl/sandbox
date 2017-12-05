603e51b71349c8f8255e16d0b4ccffac4c4bb9ac
Mark Little <mlittle@redhat.com>
2017-12-05 16:08:38 +0000

    Record list and iterator.

---
+++ Arjuna/versions/PR3.3.3_98_01_10/ClassLib/src/RecListI.cc
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: RecListI.cc,v 1.2 1995/06/26 13:13:46 ngdp Exp $
+ */
+
+#ifndef RECLIST_H_
+#  include "RecList.h"
+#endif
+
+#ifndef RECLISTI_H_
+#  include "RecListI.h"
+#endif
+
+static const char RCSid[] = "$Id: RecListI.cc,v 1.2 1995/06/26 13:13:46 ngdp Exp $";
+
+#ifdef NO_INLINES
+#  define RECLISTI_CC_
+#  include "RecListI.n"
+#  undef RECLISTI_CC_
+#endif
