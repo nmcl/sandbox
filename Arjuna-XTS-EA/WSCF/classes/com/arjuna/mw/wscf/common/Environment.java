6dc687141153ce823a201a07ad661f51f1c4e76b
Mark Little <mlittle@redhat.com>
2017-12-21 07:44:31 +0000

    environment properties

---
+++ Arjuna-XTS-EA/WSCF/classes/com/arjuna/mw/wscf/common/Environment.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2002,
+ *
+ * Arjuna Technologies Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.
+ *
+ * $Id: Environment.java,v 1.4 2003/02/13 11:28:41 nmcl Exp $
+ */
+
+package com.arjuna.mw.wscf.common;
+
+/**
+ *
+ * @author Mark Little (mark.little@arjuna.com)
+ * @version $Id: Environment.java,v 1.4 2003/02/13 11:28:41 nmcl Exp $
+ * @since 1.0.
+ */
+
+public class Environment
+{
+
+    public static final String PROTOCOL_IMPLEMENTATION = "com.arjuna.mw.wscf.protocolImplementation";
+    public static final String JTA_CONTEXT = "com.arjuna.mw.wscf.protocols.jta.context";
+    public static final String TWO_PHASE_CONTEXT = "com.arjuna.mw.wscf.protocols.twophase.context";
+    
+}
