e052e246b2f38d3db362239fa69ada2349f1ff33
Mark Little <mlittle@redhat.com>
2017-12-22 07:12:11 +0000

    More protocol messages.

---
+++ Arjuna-XTS-EA/WSCF/classes/com/arjuna/mw/wscf/model/twophase/status/Prepared.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2002,
+ *
+ * Arjuna Technologies Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.
+ *
+ * $Id: Prepared.java,v 1.1 2003/01/07 10:33:46 nmcl Exp $
+ */
+
+package com.arjuna.mw.wscf.model.twophase.status;
+
+import com.arjuna.mw.wsas.status.Status;
+
+/**
+ * The coordinator has prepared.
+ *
+ * @author Mark Little (mark@arjuna.com)
+ * @version $Id: Prepared.java,v 1.1 2003/01/07 10:33:46 nmcl Exp $
+ * @since 1.0.
+ */
+
+public class Prepared implements Status
+{
+
+    public static Prepared instance ()
+    {
+	return _instance;
+    }
+    
+    public String toString ()
+    {
+	return "org.w3c.wscf.twophase.status.Prepared";
+    }
+
+    private Prepared ()
+    {
+    }
+
+    private static final Prepared _instance = new Prepared();
+    
+}
---
+++ Arjuna-XTS-EA/WSCF/classes/com/arjuna/mw/wscf/model/twophase/status/Preparing.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2002,
+ *
+ * Arjuna Technologies Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.
+ *
+ * $Id: Preparing.java,v 1.1 2003/01/07 10:33:46 nmcl Exp $
+ */
+
+package com.arjuna.mw.wscf.model.twophase.status;
+
+import com.arjuna.mw.wsas.status.Status;
+
+/**
+ * The coordinator is preparing.
+ *
+ * @author Mark Little (mark@arjuna.com)
+ * @version $Id: Preparing.java,v 1.1 2003/01/07 10:33:46 nmcl Exp $
+ * @since 1.0.
+ */
+
+public class Preparing implements Status
+{
+
+    public static Preparing instance ()
+    {
+	return _instance;
+    }
+    
+    public String toString ()
+    {
+	return "org.w3c.wscf.twophase.status.Preparing";
+    }
+
+    private Preparing ()
+    {
+    }
+
+    private static final Preparing _instance = new Preparing();
+    
+}
