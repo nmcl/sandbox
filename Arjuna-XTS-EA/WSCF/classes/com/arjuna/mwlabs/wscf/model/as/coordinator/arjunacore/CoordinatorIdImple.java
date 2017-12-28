74c75968e4d040fe130a718adcc4c09783beb500
Mark Little <mlittle@redhat.com>
2017-12-28 11:00:37 +0000

    More ArjunaCore coordinator code

---
+++ Arjuna-XTS-EA/WSCF/classes/com/arjuna/mwlabs/wscf/model/as/coordinator/arjunacore/CoordinatorIdImple.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2002,
+ *
+ * Arjuna Technologies Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.
+ *
+ * $Id: CoordinatorIdImple.java,v 1.1 2003/01/07 10:33:52 nmcl Exp $
+ */
+
+package com.arjuna.mwlabs.wscf.model.as.coordinator.arjunacore;
+
+import com.hp.mw.ts.arjuna.common.Uid;
+
+import com.arjuna.mw.wscf.common.CoordinatorId;
+
+/**
+ * This implementation of CoordinatorId uses the ArjunaCore Uid class.
+ *
+ * @author Mark Little (mark.little@arjuna.com)
+ * @version $Id: CoordinatorIdImple.java,v 1.1 2003/01/07 10:33:52 nmcl Exp $
+ * @since 1.0.
+ */
+
+public class CoordinatorIdImple extends Uid implements CoordinatorId
+{
+
+    public CoordinatorIdImple ()
+    {
+	super();
+
+	_value = stringForm().getBytes();
+    }
+    
+    public CoordinatorIdImple (String id)
+    {
+	super(id);
+
+	_value = stringForm().getBytes();
+    }
+
+    public CoordinatorIdImple (Uid id)
+    {
+	super(id);
+
+	_value = stringForm().getBytes();
+    }
+    
+    public byte[] value ()
+    {
+	return _value;
+    }
+    
+    private byte[] _value;
+    
+}
+
