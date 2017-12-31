8ee9582975e812f3e40ae14220994699cb761fef
Mark Little <mlittle@redhat.com>
2017-12-31 14:37:29 +0000

    Begin-then-cancel unit test

---
+++ Arjuna-XTS-EA/WSCF/classes/com/arjuna/mwtests/wscf/model/twophase/BeginCancel.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2002,
+ *
+ * Arjuna Technologies Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.
+ *
+ * $Id: BeginCancel.java,v 1.1 2003/01/07 10:34:00 nmcl Exp $
+ */
+
+package com.arjuna.mwtests.wscf.model.twophase;
+
+import com.arjuna.mwlabs.wscf.utils.ProtocolLocator;
+
+import com.arjuna.mw.wscf.model.twophase.api.UserCoordinator;
+
+import com.arjuna.mw.wscf.model.twophase.UserCoordinatorFactory;
+
+import com.arjuna.mw.wscf.model.twophase.common.*;
+import com.arjuna.mw.wscf.model.twophase.exceptions.*;
+
+import com.arjuna.mw.wscf.common.*;
+
+import com.arjuna.mw.wsas.activity.*;
+
+import com.arjuna.mw.wsas.context.Context;
+import com.arjuna.mw.wsas.context.DeploymentContext;
+import com.arjuna.mw.wsas.context.DeploymentContextFactory;
+import com.arjuna.mw.wsas.context.soap.SOAPContext;
+
+import com.arjuna.mw.wsas.exceptions.NoActivityException;
+
+/**
+ * @author Mark Little (mark.little@arjuna.com)
+ * @version $Id: BeginCancel.java,v 1.1 2003/01/07 10:34:00 nmcl Exp $
+ * @since 1.0.
+ */
+
+public class BeginCancel
+{
+
+    public static void main (String[] args)
+    {
+	boolean passed = false;
+	
+	try
+	{
+	    UserCoordinator ua = UserCoordinatorFactory.userCoordinator();
+	    
+	    ua.begin();
+
+	    System.out.println("Started: "+ua.identifier()+"\n");
+
+	    ua.cancel();
+
+	    passed = true;
+	}
+	catch (Exception ex)
+	{
+	    ex.printStackTrace();
+	}
+	
+	if (passed)
+	    System.out.println("\nPassed.");
+	else
+	    System.out.println("\nFailed.");
+    }
+
+}
