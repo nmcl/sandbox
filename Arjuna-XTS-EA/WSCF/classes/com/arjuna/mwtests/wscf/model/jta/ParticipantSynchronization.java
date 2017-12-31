a1aebbf1c0611d2a33b73a73b4d1e0719d0e6942
Mark Little <mlittle@redhat.com>
2017-12-31 14:35:44 +0000

    synchronisation unit test

---
+++ Arjuna-XTS-EA/WSCF/classes/com/arjuna/mwtests/wscf/model/jta/ParticipantSynchronization.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2002,
+ *
+ * Arjuna Technologies Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.
+ *
+ * $Id: ParticipantSynchronization.java,v 1.1 2003/01/07 10:33:59 nmcl Exp $
+ */
+
+package com.arjuna.mwtests.wscf.model.jta;
+
+import com.arjuna.mwtests.wscf.common.*;
+
+import com.arjuna.mw.wscf.model.xa.*;
+
+import com.arjuna.mw.wsas.activity.*;
+
+import com.arjuna.mw.wsas.exceptions.NoActivityException;
+
+/**
+ * @author Mark Little (mark.little@arjuna.com)
+ * @version $Id: ParticipantSynchronization.java,v 1.1 2003/01/07 10:33:59 nmcl Exp $
+ * @since 1.0.
+ */
+
+public class ParticipantSynchronization
+{
+
+    public static void main (String[] args)
+    {
+	boolean passed = false;
+	
+	try
+	{
+	    javax.transaction.TransactionManager tm = TransactionManagerFactory.transactionManager();
+	    
+	    tm.begin();
+
+	    tm.getTransaction().enlistResource(new DemoXAResource());
+	    tm.getTransaction().registerSynchronization(new JTASynchronization());
+	    tm.getTransaction().registerSynchronization(new JTASynchronization());
+	    
+	    System.out.println("Started: "+tm+"\n");
+
+	    tm.commit();
+
+	    passed = true;
+	}
+	catch (Exception ex)
+	{
+	    ex.printStackTrace();
+	}
+	
+	if (passed)
+	    System.out.println("\nPassed.");
+	else
+	    System.out.println("\nFailed.");
+    }
+
+}
