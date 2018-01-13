c5ddd148ae657a1cdc7decfcbc803e216914baa4
Mark Little <mlittle@redhat.com>
2018-01-13 14:28:19 +0000

    configuration classes

---
+++ Arjuna-XTS-EA/WSTX/classes/com/arjuna/mw/wstx/common/Environment.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2002,
+ *
+ * Arjuna Technologies Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.
+ *
+ * $Id: Environment.java,v 1.1 2002/11/25 11:00:51 nmcl Exp $
+ */
+
+package com.arjuna.mw.wstx.common;
+
+/**
+ */
+
+public class Environment
+{
+}
---
+++ Arjuna-XTS-EA/WSTX/classes/com/arjuna/mw/wstx/common/TransactionXSD.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2002,
+ *
+ * Arjuna Technologies Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.
+ *
+ * $Id: TransactionXSD.java,v 1.1 2003/02/03 11:01:27 nmcl Exp $
+ */
+
+package com.arjuna.mw.wstx.common;
+
+/**
+ * @author Mark Little (mark.little@arjuna.com)
+ * @version $Id: TransactionXSD.java,v 1.1 2003/02/03 11:01:27 nmcl Exp $
+ * @since 1.0.
+ */
+
+public class TransactionXSD
+{
+    
+    public static final String transactionType = "transaction-type";
+    public static final String transactionProtocol = "transaction-protocol";
+    
+}
