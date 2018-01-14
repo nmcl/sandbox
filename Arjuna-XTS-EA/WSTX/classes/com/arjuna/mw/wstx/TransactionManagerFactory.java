ed1b90cd43d5a6b5226405615ef40607bea601a1
Mark Little <mlittle@redhat.com>
2018-01-14 19:22:56 +0000

    Transaction manager and factory.

---
+++ Arjuna-XTS-EA/WSTX/classes/com/arjuna/mw/wstx/TransactionManagerFactory.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2002,
+ *
+ * Arjuna Technologies Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.
+ *
+ * $Id: TransactionManagerFactory.java,v 1.4 2003/03/14 09:55:23 nmcl Exp $
+ */
+
+package com.arjuna.mw.wstx;
+
+import com.arjuna.mw.wstx.common.TransactionXSD;
+
+import com.arjuna.mw.wscf.protocols.*;
+import com.arjuna.mw.wscf.utils.*;
+
+import com.arjuna.mw.wscf.exceptions.ProtocolNotRegisteredException;
+
+import com.arjuna.mwlabs.wscf.utils.ProtocolLocator;
+
+import com.arjuna.mw.wsas.exceptions.SystemException;
+
+import java.util.HashMap;
+
+/**
+ * This class is used to obtain the TransactionManager implementation to
+ * use.
+ *
+ * @author Mark Little (mark.little@arjuna.com)
+ * @version $Id: TransactionManagerFactory.java,v 1.4 2003/03/14 09:55:23 nmcl Exp $
+ * @since 1.0.
+ */
+
+public class TransactionManagerFactory
+{
+
+    /**
+     * @exception ProtocolNotRegisteredException Thrown if the default
+     * protocol is not available.
+     *
+     * @return the UserCoordinator implementation to use. The default
+     * coordination protocol is used (two-phase commit) with its
+     * associated implementation.
+     */
+
+    public static TransactionManager transactionManager () throws ProtocolNotRegisteredException, SystemException
+    {
+	try
+	{
+	    ProtocolLocator pl = new ProtocolLocator(com.arjuna.mwlabs.wstx.model.as.twophase.TwoPhaseTxManager.class.getName());
+	    org.w3c.dom.Document doc = pl.getProtocol();
+	    
+	    if (doc == null)
+	    {
+		System.err.println("Failed to create "+com.arjuna.mwlabs.wstx.model.as.twophase.TwoPhaseTxManager.class.getName());
+	    }
+	    else
+	    {
+		if (!_protocolManager.present(doc))
+		    _protocolManager.addProtocol(doc, com.arjuna.mwlabs.wstx.model.as.twophase.TwoPhaseTxManager.class.getName());
+		
+		return transactionManager(doc);
+	    }
+	}
+	catch (Exception ex)
+	{
+	    ex.printStackTrace();
+
+	    throw new SystemException(ex.toString());
+	}
+
+	return null;
+    }
+
+    /**
+     * Obtain a reference to a coordinator that implements the specified
+     * protocol.
+     *
+     * @param org.w3c.dom.Document protocol The XML definition of the type of
+     * coordination protocol required.
+     *
+     * @exception ProtocolNotRegisteredException Thrown if the requested
+     * protocol is not available.
+     *
+     * @return the UserCoordinator implementation to use.
+     */
+    
+    /*
+     * Have the type specified in XML. More data may be specified, which
+     * can be passed to the implementation in the same way ObjectName was.
+     */
+
+    public static TransactionManager transactionManager (org.w3c.dom.Document protocol) throws ProtocolNotRegisteredException, SystemException
+    {
+	try
+	{
+	    synchronized (_implementations)
+	    {
+		org.w3c.dom.Text child = DomUtil.getTextNode(protocol, TransactionXSD.transactionType);
+		String protocolType = child.getNodeValue();
+		TransactionManager tx = (TransactionManager) _implementations.get(protocolType);
+	
+		if (tx == null)
+		{
+		    Object implementation = _protocolManager.getProtocolImplementation(protocol);
+	
+		    if (implementation instanceof String)
+		    {
+			Class txImpl = Class.forName((String) implementation);
+
+			tx = (TransactionManager) txImpl.newInstance();
+		    }
+		    else
+			tx = (TransactionManager) implementation;
+
+		    _implementations.put(protocolType, tx);
+		}
+
+		return tx;
+	    }
+	}
+	catch (ProtocolNotRegisteredException ex)
+	{
+	    throw ex;
+	}
+	catch (Exception ex)
+	{
+	    ex.printStackTrace();
+	    
+	    throw new SystemException(ex.toString());
+	}
+    }
+
+    private static ProtocolManager _protocolManager = ProtocolRegistry.sharedManager();
+    private static HashMap         _implementations = new HashMap();
+    
+}
+
