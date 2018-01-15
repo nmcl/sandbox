9ebdfdcb89b7165293070ad3311abdbb3d40d261
Mark Little <mlittle@redhat.com>
2018-01-15 19:56:14 +0000

    Exception protocol messages.

---
+++ Arjuna-XTS-EA/WSTX/classes/com/arjuna/mwtests/wst/basic/CommitExceptionInPrepare.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2002,
+ *
+ * Arjuna Technologies Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.
+ *
+ * $Id: CommitExceptionInPrepare.java,v 1.6 2003/03/14 09:55:24 nmcl Exp $
+ */
+
+package com.arjuna.mwtests.wst.basic;
+
+import com.arjuna.mw.wst.*;
+
+import com.arjuna.wst.*;
+
+import com.arjuna.mwtests.wst.common.*;
+
+/**
+ * @author Mark Little (mark.little@arjuna.com)
+ * @version $Id: CommitExceptionInPrepare.java,v 1.6 2003/03/14 09:55:24 nmcl Exp $
+ * @since 1.0.
+ */
+
+public class CommitExceptionInPrepare
+{
+
+    public static void main (String[] args)
+    {
+	boolean passed = false;
+	
+	try
+	{
+	    UserTransaction ut = UserTransactionFactory.userTransaction();
+	    TransactionManager tm = TransactionManagerFactory.transactionManager();
+	    FailureParticipant p1 = new FailureParticipant(FailureParticipant.FAIL_IN_PREPARE, FailureParticipant.WRONG_STATE);
+	    DemoParticipant p2 = new DemoParticipant();
+	    
+	    ut.begin();
+	    
+	    tm.enlistForTwoPhase(p1, null);
+	    tm.enlistForTwoPhase(p2, null);
+	    
+	    ut.commit();
+
+	    passed = false;
+	}
+	catch (com.arjuna.wst.SystemException ex)
+	{
+	    passed = true;
+	}
+	catch (Exception ex)
+	{
+	    ex.printStackTrace();
+	}
+	
+	if (passed)
+	    System.out.println("\nPassed.");
+	else
+	    System.out.println("\nFailed.");
+    }
+
+}
