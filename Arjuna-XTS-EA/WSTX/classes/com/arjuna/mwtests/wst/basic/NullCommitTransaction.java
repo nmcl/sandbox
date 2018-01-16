c190814787bbe5f903da84dd5fdedb6257cec91b
Mark Little <mlittle@redhat.com>
2018-01-16 16:34:43 +0000

    Null transaction tests.

---
+++ Arjuna-XTS-EA/WSTX/classes/com/arjuna/mwtests/wst/basic/NullCommitTransaction.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2002,
+ *
+ * Arjuna Technologies Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.
+ *
+ * $Id: NullCommitTransaction.java,v 1.3 2003/02/13 11:45:55 nmcl Exp $
+ */
+
+package com.arjuna.mwtests.wst.basic;
+
+import com.arjuna.mw.wst.UserTransaction;
+import com.arjuna.mw.wst.UserTransactionFactory;
+
+/**
+ * @author Mark Little (mark.little@arjuna.com)
+ * @version $Id: NullCommitTransaction.java,v 1.3 2003/02/13 11:45:55 nmcl Exp $
+ * @since 1.0.
+ */
+
+public class NullCommitTransaction
+{
+
+    public static void main (String[] args)
+    {
+	boolean passed = false;
+	
+	try
+	{
+	    UserTransaction ut = UserTransactionFactory.userTransaction();
+	
+	    ut.begin();
+	    
+	    ut.commit();
+
+	    passed = true;
+	}
+	catch (Exception ex)
+	{
+	    ex.printStackTrace();
+	}
+	
+	if (passed)
+	    System.out.println("\nPassed.");
+	else
+	    System.out.println("\nFailed.");
+    }
+
+}
