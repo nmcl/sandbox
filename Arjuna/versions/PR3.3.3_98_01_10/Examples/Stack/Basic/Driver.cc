332ed2baccc4021a6b5571508703c100a07d7126
Mark Little <mlittle@redhat.com>
2018-02-12 02:29:29 +0000

    basic example

---
+++ Arjuna/versions/PR3.3.3_98_01_10/Examples/Stack/Basic/Driver.cc
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ */
+
+/*
+ *  Stack drived program. (Basic)
+ */
+
+#include <iostream.h>
+
+#include "Stack.h"
+
+void Message(const char* text, const enum stackOutcome outcome)
+{
+    cout << text << " : ";
+    switch (outcome)
+    {
+        case unknown:
+	    cout << "unknown\n" << flush;
+	    break;
+	case stack_full:
+	    cout << "stack full\n" << flush;
+	    break;
+	case stack_empty:
+	    cout << "stack empty\n" << flush;
+	    break;
+	case not_done:
+	    cout << "not done\n" << flush;
+	    break;
+	case done:
+	    cout << "done\n" << flush;
+	    break;
+	default:
+	    cout << "** unknown **\n" << flush;
+	    break;
+    }
+}
+
+int main(int, char *[])
+{
+    int               value;
+    enum stackOutcome outcome;
+
+    outcome = unknown;
+    Stack stack(outcome);
+    Message("Constructor for Stack object", outcome);
+
+    outcome = unknown;
+    value = 0;
+    stack.Pop(outcome, value);
+    Message("Pop", outcome);
+    cout << "Value returned is " << value << "\n" << flush;
+
+    outcome = unknown;
+    value = 12;
+    stack.Push(outcome, value);
+    Message("Push of 12", outcome);
+
+    outcome = unknown;
+    value = 24;
+    stack.Push(outcome, value);
+    Message("Push of 24", outcome);
+
+    outcome = unknown;
+    value = 34;
+    stack.Push(outcome, value);
+    Message("Push of 34", outcome);
+
+    outcome = unknown;
+    value = 0;
+    stack.Pop(outcome, value);
+    Message("Pop", outcome);
+    cout << "Value returned is " << value << "\n" << flush;
+
+    outcome = unknown;
+    value = 0;
+    stack.Pop(outcome, value);
+    Message("Pop", outcome);
+    cout << "Value returned is " << value << "\n" << flush;
+
+    return 0;
+}
