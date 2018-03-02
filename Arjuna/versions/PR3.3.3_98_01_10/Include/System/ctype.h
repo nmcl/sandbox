b04a16387efe87ca25f74103c25a7686163643e1
Mark Little <mlittle@redhat.com>
2018-03-02 12:33:33 +0000

    Some system header files.

---
+++ Arjuna/versions/PR3.3.3_98_01_10/Include/System/ctype.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: ctype.h,v 1.1 1993/11/03 14:32:38 nmcl Exp $
+ */
+
+#ifndef CTYPE_H_
+#define CTYPE_H_
+
+#ifdef STUB
+#  pragma @NoRename
+#endif
+
+#include <ctype.h>
+
+#endif
---
+++ Arjuna/versions/PR3.3.3_98_01_10/Include/System/errno.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: errno.h,v 1.1 1993/11/03 14:32:41 nmcl Exp $
+ */
+
+#ifndef ERRNO_H_
+#define ERRNO_H_
+
+#ifdef STUB
+#  pragma @NoRename
+#endif
+
+#include <errno.h>
+
+#endif
