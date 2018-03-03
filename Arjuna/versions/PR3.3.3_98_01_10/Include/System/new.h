88f2ce3013b5a17ab0f194c0fd76dd0d8e90a414
Mark Little <mlittle@redhat.com>
2018-03-03 14:33:31 +0000

    system header files.

---
+++ Arjuna/versions/PR3.3.3_98_01_10/Include/System/new.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: new.h,v 1.1 1993/11/03 14:32:55 nmcl Exp $
+ */
+
+#ifndef NEW_H_
+#define NEW_H_
+
+#ifdef STUB
+#  pragma @NoRename
+#endif
+
+#include <new.h>
+
+#endif
---
+++ Arjuna/versions/PR3.3.3_98_01_10/Include/System/osfcn.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: osfcn.h,v 1.1 1993/11/03 14:32:56 nmcl Exp $
+ */
+
+#ifndef OSFCN_H_
+#define OSFCN_H_
+
+#ifdef STUB
+#  pragma @NoRename
+#endif
+
+#include <osfcn.h>
+
+#endif
