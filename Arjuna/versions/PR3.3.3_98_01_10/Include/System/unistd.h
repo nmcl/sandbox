c465b1a2ac8ff9100f1bb1632b8ddd86abf645d1
Mark Little <mlittle@redhat.com>
2018-03-06 07:59:26 +0000

    system header files.

---
+++ Arjuna/versions/PR3.3.3_98_01_10/Include/System/unistd.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: unistd.h,v 1.2.6.1 1996/10/10 12:26:03 ngdp Exp $
+ */
+
+#ifndef UNISTD_H_
+#define UNISTD_H_
+
+#ifdef STUB
+#  pragma @NoRename
+#endif
+
+#ifndef WIN32
+#  include <unistd.h>
+#else
+#  include <process.h>
+#endif
+
+#ifdef NEED_USLEEP_PROTO
+extern "C"
+{
+    int usleep(unsigned int);
+}
+#endif
+
+#if defined(sun) && !defined(SVR4)
+extern "C"
+{
+    int getpagesize ();
+}
+#endif
+
+/*
+ * Some machines have different names for the same thing...
+ */
+
+#ifndef _SC_PAGESIZE
+#  if defined _SC_PAGE_SIZE
+#    define _SC_PAGESIZE _SC_PAGE_SIZE
+#  endif
+#endif
+
+#endif
