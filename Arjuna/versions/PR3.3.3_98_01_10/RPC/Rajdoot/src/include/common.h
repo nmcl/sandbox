3c3371939293ffb3a4af32ee37a271e1e52d7c02
Mark Little <mlittle@redhat.com>
2018-03-17 13:44:25 +0000

    common types

---
+++ Arjuna/versions/PR3.3.3_98_01_10/RPC/Rajdoot/src/include/common.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ */
+
+/*
+ * $Id: common.h,v 1.2 1994/12/09 11:38:41 ngdp Exp $
+ */
+
+#ifndef RAJ_COMMON_H_
+#define RAJ_COMMON_H_
+
+#ifndef COMMONT_H_
+#  include <Common/CommonT.h>
+#endif
+
+extern Uint32 get_machine_id();
+
+#endif
