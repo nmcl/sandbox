794658da8f2c027a5340ff05e2ea4253817ddb5b
Mark Little <mlittle@redhat.com>
2018-03-21 14:58:58 +0000

    Stub generator cpp directive handling.

---
+++ Arjuna/versions/PR3.3.3_98_01_10/StubGen/include/DirectiveT.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: DirectiveT.h,v 1.1 1993/11/03 14:44:52 nmcl Exp $
+ */
+
+#ifndef DIRECTIVET_H_
+#define DIRECTIVET_H_
+
+/*
+ *
+ * Stub generator cpp directive handling. Treats directives as
+ * instances of a special type for consistency with other
+ * forms of type manipulation .
+ *
+ */
+
+#ifndef STCOMMONT_H_
+#  include "StCommonT.h"
+#endif
+
+#ifndef STRING_H_
+#  include "String.h"
+#endif
+
+#ifndef DUMMYT_H_
+#  include "DummyT.h"
+#endif
+
+#ifndef DECLSTYLE_H_
+#  include "DeclStyle.h"
+#endif
+
+class Directive : public DummyType
+{
+public:
+    Directive (const String&);
+    virtual ~Directive ();
+
+    virtual String postName (DeclStyle) const;
+    virtual String preName (DeclStyle = NORMAL_DECL, Boolean = FALSE,
+			    Boolean = FALSE) const;
+
+private:
+    String directive;
+};
+
+#endif
