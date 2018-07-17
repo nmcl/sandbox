0329fd7fe2e6e8323d8c288c5a0d2b0f67b9dfce
nmcl <mlittle@redhat.com>
2018-07-17 17:42:43 +0100

    stub gen include files

---
+++ W3OTrans/StubGen.java/StubGen/include/BinExpr.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 1993
+ *
+ * Department of Computing Science,
+ * The University,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: BinExpr.h,v 1.2 1994/07/25 13:26:57 ngdp Exp $
+ */
+
+#ifndef BINEXPR_H_
+#define BINEXPR_H_
+
+/*
+ *
+ * Simple expression handling. Used as the basis for initial values.
+ * This is a binary expression handler. Used for unary expressions too
+ * by setting either left or right to Null.
+ *
+ */
+
+#ifndef STRING_H_
+#  include "String.h"
+#endif
+
+#ifndef EXPRESSION_H_
+#  include "Expression.h"
+#endif
+
+class ostream;
+
+class BinaryExpression : public Expression
+{
+public:
+    BinaryExpression (const String&, Expression*, Expression*);
+
+    virtual ostream& printExpr (ostream&) const;
+private:
+    String exprOperator;
+    Expression *leftOperand;
+    Expression *rightOperand;
+
+};
+
+#include "BinExpr.n"
+
+#endif
