28d912db51f423cb500f0b511306a0a5c12aae96
Mark Little <markclittle@gmail.com>
2018-12-29 18:47:35 +0000

    Use JAVAARJUNA_HOME
    https://github.com/nmcl/sandbox/issues/73

---
+++ JavaArjuna/v0.1.0/classes/com/arjuna/JavaArjuna/Environment.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 1996, 1997, 1998, 1999,
+ *
+ * Department of Computing Science,
+ * University of Newcastle upon Tyne,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id$
+ */
+
+package com.arjuna.JavaArjuna;
+
+public class Environment
+{
+    public static String JAVAARJUNA_HOME = "JAVAARJUNA_HOME";
+    public static String PROPERTY_FILE_NAME = "properties.txt";
+}
+
