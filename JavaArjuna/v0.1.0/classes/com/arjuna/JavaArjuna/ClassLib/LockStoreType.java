46d328d01f6a6af4fd10747164822bb092f4a7a7
Mark Little <markclittle@gmail.com>
2018-12-30 21:23:50 +0000

    Initial type allocation work
    https://github.com/nmcl/sandbox/issues/79

---
+++ JavaArjuna/v0.1.0/classes/com/arjuna/JavaArjuna/ClassLib/LockStoreType.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 1996, 1997, 1998,
+ *
+ * Department of Computing Science,
+ * University of Newcastle upon Tyne,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id: RecordType.java,v 1.4 1998/07/06 13:26:30 nmcl Exp $
+ */
+
+package com.arjuna.JavaArjuna.ClassLib;
+
+/*
+ * Maintain the types of LockStore implementations.
+ */
+
+public class LockStoreType
+{
+};
