017cc781525d4fc9fa27cc85bcfdaee19ba233aa
Mark Little <markclittle@gmail.com>
2019-01-02 16:46:58 +0000

    Initial ObjectStore type manipulation.
    https://github.com/nmcl/sandbox/issues/79

---
+++ JavaArjuna/v0.1.0/classes/com/arjuna/JavaArjuna/ObjectStore/ObjectStoreType.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 1996, 1997, 1998,
+ *
+ * Department of Computing Science,
+ * University of Newcastle upon Tyne,
+ * Newcastle upon Tyne,
+ * UK.
+ *
+ * $Id$
+ */
+
+package com.arjuna.JavaArjuna.ObjectStore;
+
+/*
+ * Maintain the types of ObjectStore implementations.
+ */
+
+public class LockStoreType
+{
+    public static String SHADOWING_STORE = "ShadowingStore";
+};
