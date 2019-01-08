7d8a635c42465befa19bf5fc6339f50816349c88
Mark Little <mlittle@redhat.com>
2019-01-08 16:18:17 +0000

    Removed Gandiva code.
    https://github.com/nmcl/sandbox/issues/87

--- JavaArjuna/v0.1.0/tests/ConcurrencyControl/ConcurrencyTest.java
+++ JavaArjuna/v0.1.0/tests/ConcurrencyControl/ConcurrencyTest.java
@@ -9,11 +9,11 @@
  * $Id: ConcurrencyTest.java,v 1.1 1998/11/12 09:39:12 nmcl Exp $
  */
 
-import JavaArjunaLiteTests.AtomicObject;
-import com.arjuna.JavaArjunaLite.Atomic.*;
-import com.arjuna.JavaGandiva.Common.*;
+import JavaArjunaTests.AtomicObject;
+import com.arjuna.JavaArjuna.ClassLib.*;
+import com.arjuna.JavaArjuna.Common.*;
 
-import JavaArjunaLiteTests.TestException;
+import JavaArjunaTests.TestException;
 
 public class ConcurrencyTest
 {
