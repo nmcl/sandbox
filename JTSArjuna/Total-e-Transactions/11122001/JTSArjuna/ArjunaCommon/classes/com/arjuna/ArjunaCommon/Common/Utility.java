3d64d65e23c2450e8a238d7540b99f17824e0a29
Mark Little <mlittle@redhat.com>
2019-08-08 07:54:57 -0400

    Create Utility.java

---
+++ JTSArjuna/Total-e-Transactions/11122001/JTSArjuna/ArjunaCommon/classes/com/arjuna/ArjunaCommon/Common/Utility.java
@@ -0,0 +1,274 @@
+/*
+ * Copyright (C) 1998, 1999, 2000, 2001,
+ *
+ * Arjuna Solutions Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.  
+ *
+ * $Id: Utility.java,v 1.1.10.1.20.1.2.1.2.1.4.3.32.1.4.3.18.4 2001/11/02 10:18:05 jcoleman Exp $
+ */
+
+package com.arjuna.ArjunaCommon.Common;
+
+import java.util.Properties;
+import java.io.*;
+import java.net.InetAddress;
+
+import java.net.UnknownHostException;
+import java.lang.NumberFormatException;
+import java.lang.StringIndexOutOfBoundsException;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+/**
+ * Various useful functions that we wrap in a single class.
+ * Some of these functions are needed simply for backwards
+ * compatibility with older versions of Java.
+ *
+ * @author Mark Little (mark@arjuna.com)
+ * @version $Id: Utility.java,v 1.1.10.1.20.1.2.1.2.1.4.3.32.1.4.3.18.4 2001/11/02 10:18:05 jcoleman Exp $
+ * @since JTS 1.0.
+ */
+
+public class Utility
+{
+
+    /**
+     * Convert integer to hex String.
+     */
+    
+public static String intToHexString (int number) throws NumberFormatException
+    {
+	return Integer.toString(number, 16);
+    }
+
+    /**
+     * Convert a hex String to an integer.
+     *
+     * Be careful of -1. Java IO is really bad!
+     */
+
+public static int hexStringToInt (String s) throws NumberFormatException
+    {
+	boolean isNeg;
+	String toUse = s;
+	
+	if (s.startsWith(Utility.hexStart))
+	    toUse = s.substring(Utility.hexStart.length());
+
+	String lastString = toUse.substring(toUse.length()-1);
+
+	if (toUse.substring(0, 1).equals("-")) {
+		toUse = "-0" + toUse.substring(1, toUse.length() - 1);
+		isNeg = true;
+	} else {
+		toUse = "0" + toUse.substring(0, toUse.length() - 1);
+		isNeg = false;
+	}
+
+	Integer i = Integer.valueOf(toUse, 16);
+
+	int val = i.intValue();
+	
+	val = val << 4;
+	
+	if (isNeg) {
+		val -= Integer.valueOf(lastString, 16).intValue();
+	} else {
+		val += Integer.valueOf(lastString, 16).intValue();
+	}
+
+	return val;
+    }
+
+    /**
+     * Convert a long to a hex String.
+     */
+
+public static String longToHexString (long number) throws NumberFormatException
+    {
+	return Long.toString(number, 16);
+    }
+
+    /**
+     * Convert a hex String to a long.
+     */
+
+public static long hexStringToLong (String s) throws NumberFormatException
+    {
+	boolean isNeg;
+	String toUse = s;
+	
+	if (s.startsWith(Utility.hexStart))
+	    toUse = s.substring(Utility.hexStart.length());
+
+	String lastString = toUse.substring(toUse.length()-1);
+
+	if (toUse.substring(0, 1).equals("-")) {
+		toUse = "-0" + toUse.substring(1, toUse.length() - 1);
+		isNeg = true;
+	} else {
+		toUse = "0" + toUse.substring(0, toUse.length() - 1);
+		isNeg = false;
+	}
+
+	Long i = Long.valueOf(toUse, 16);
+
+	long val = i.longValue();
+	
+	val = val << 4;
+	
+	if (isNeg) {
+		val -= Long.valueOf(lastString, 16).longValue();
+	} else {
+		val += Long.valueOf(lastString, 16).longValue();
+	}
+
+	return val;
+    }    
+
+    /**
+     * @return an integer representing the ip address of the local
+     * machine. Essentially the bytes of the InetAddress are shuffled
+     * into the integer.
+     *
+     * This was once part of the Uid class but has been separated for
+     * general availability.
+     *
+     * @since JTS 2.1.
+     */
+
+public static synchronized int hostInetAddr () throws UnknownHostException
+    {
+	/*
+	 * Calculate only once.
+	 */
+
+	if (myAddr == 0)
+	{
+	    InetAddress addr = InetAddress.getLocalHost();
+	    byte[] b = addr.getAddress();
+	
+	    for (int i = 0; i < b.length; i++)
+	    {
+		/*
+		 * Convert signed byte into unsigned.
+		 */
+		
+		int l = 0x7f & b[i];
+
+		l += (0x80 & b[i]);
+		    
+		myAddr = (myAddr << 8) | l;
+	    }
+	}
+
+	return myAddr;
+    }
+
+    /**
+     * @return the process id. This had better be unique between processes
+     * on the same machine. If not we're in trouble!
+     *
+     * @since JTS 2.1.
+     */
+    
+public static final int getpid ()
+    {
+	if (Utility.processId == 0)
+	{
+	synchronized (Utility.hexStart)
+	{
+	    /*
+	     * All of this is just to ensure uniqueness!
+	     */
+
+	    if (Utility.processId == 0)	
+	    {
+		int retry = 1000;
+		int pid = (int) System.currentTimeMillis();
+		String dir = PropertyManager.getProperty(Environment.VAR_DIR);
+
+		pid = Math.abs(pid);
+		    
+		if (dir == null)
+		{
+		    ClassPathParser cp = new ClassPathParser();
+
+		    dir = cp.getPath(File.separator + "var" + File.separator + "tmp");
+
+		    if (dir == null)
+		    {
+			if (Utility.isWindows())  // cygwin issue
+			    dir = cp.getPath("/var/tmp");
+		    }
+
+		    if (dir == null)
+			throw new FatalError("Utility.getpid - could not locate temporary directory. Ensure it is on your CLASSPATH.");
+		}
+		else
+		    dir = dir + File.separator + "tmp";
+
+		for (int i = 0; i < retry; i++)
+		{
+		    try
+		    {
+			File f = new File(dir + File.separator+ "pid " + pid);
+
+			if (f.createNewFile())
+			{
+			    f.deleteOnExit();  // problem if we crash?
+
+			    processId = pid;
+		    
+			    break;
+			}
+			else
+			    pid++;
+		    }
+		    catch (IOException e)
+		    {
+		    }
+		}
+
+		if (processId == 0)
+		    throw new FatalError("Utility.getpid could not create unique file.");
+	    }
+	}
+	}
+
+	return processId;
+    }
+
+    /**
+     * @return a Uid representing this process.
+     *
+     * @since JTS 2.1.
+     */
+
+public static final synchronized Uid getProcessUid ()
+    {
+	if (processUid == null)
+	    processUid = new Uid();
+
+	return processUid;
+    }
+
+public static final boolean isWindows ()
+    {
+	String os = PropertyManager.getProperty("os.name");
+	
+	if ((os == "WIN32") || (os.indexOf("Windows") != -1))
+	    return true;
+	else
+	    return false;
+    }
+
+private static int myAddr = 0;
+private static int processId = 0;
+private static Uid processUid = null;
+    
+private static final String hexStart = "0x";
+    
+}
