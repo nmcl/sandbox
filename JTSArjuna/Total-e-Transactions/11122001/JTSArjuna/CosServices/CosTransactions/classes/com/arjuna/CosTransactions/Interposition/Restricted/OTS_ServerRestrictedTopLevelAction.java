c1e1d49f2dd9e1650a1d0bdcf1391159a5dda338
Mark Little <mlittle@redhat.com>
2019-08-14 15:53:57 -0400

    Restricted interposition classes

---
+++ JTSArjuna/Total-e-Transactions/11122001/JTSArjuna/CosServices/CosTransactions/classes/com/arjuna/CosTransactions/Interposition/Restricted/OTS_ServerRestrictedTopLevelAction.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 1998, 1999, 2000,
+ *
+ * Arjuna Solutions Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.
+ *
+ * $Id: OTS_ServerRestrictedTopLevelAction.java,v 1.1.8.2.8.1.14.1.2.1.2.1 2000/12/13 15:40:25 nmcl Exp $
+ */
+
+package com.arjuna.CosTransactions.Interposition.Restricted;
+
+import com.arjuna.OrbCommon.*;
+import com.arjuna.ArjunaCommon.Common.*;
+import com.arjuna.ArjunaCommon.Template.HashList;
+import com.arjuna.ArjunaCore.Atomic.PrepareOutcome;
+import com.arjuna.ArjunaCore.Atomic.ActionStatus;
+import com.arjuna.ArjunaCore.Thread.ThreadActionData;
+import com.arjuna.CosTransactions.*;
+import com.arjuna.CosTransactions.Interposition.OTS_ServerControl;
+import com.arjuna.CosTransactions.Interposition.Arjuna.OTS_ServerTopLevelAction;
+import org.omg.CosTransactions.*;
+
+/*
+ * This looks like an atomic action, but is not actually derived from
+ * BasicAction or OTS_Transaction. This is because of the way in which the
+ * OTS creates and manipulates transactions.
+ *
+ * As with OTS_Transaction, we only create actions here, and do not associated
+ * these contexts with any thread. We do the association later.
+ *
+ * If we were to do the creation via a suitably modified current interface
+ * then the thread association would be done for us automatically, and we
+ * would not have to call resume at all.
+ *
+ * This is a top-level action proxy.
+ */
+
+public class OTS_ServerRestrictedTopLevelAction extends OTS_ServerTopLevelAction
+{
+
+public OTS_ServerRestrictedTopLevelAction (OTS_ServerControl myControl)
+    {
+	super(myControl);
+
+	if (DebugController.enabled())
+	{
+	    DebugController.controller().println(DebugLevel.CONSTRUCTORS, VisibilityLevel.VIS_PUBLIC,
+					       FacilityCode.FAC_OTS, "OTS_ServerRestrictedTopLevelAction::OTS_ServerRestrictedTopLevelAction ( "+
+					       ((myControl != null) ? myControl.get_uid() : Uid.nullUid())+" )");
+	}
+    }
+
+public final synchronized OTS_ServerControl deepestControl ()
+    {
+	OTS_ServerRestrictedNestedAction myChild = child();
+
+	if (myChild != null)
+	    return myChild.deepestControl();
+	else
+	    return control();
+    }
+
+public final synchronized OTS_ServerRestrictedNestedAction child ()
+    {
+	OTS_ServerRestrictedNestedAction toReturn = null;
+	HashList children = getChildren();
+    
+	/*
+	 * There should be only one child!
+	 */
+    
+	if (children != null)
+	{
+	    if (children.size() != 1)
+	    {
+		ErrorStream.stream(ErrorStream.FATAL).println("OTS_ServerRestrictedTopLevelAction.child - found concurrent transactions!");
+
+		throw new com.arjuna.CosTransactions.OTS_Error("Concurrent children for restricted interposition!");
+	    }
+	    else
+	    {
+		toReturn = (OTS_ServerRestrictedNestedAction) children.orderedPop();
+	    }
+	}
+    
+	return toReturn;
+    }
+
+public String type ()
+    {
+	return "/Resources/Arjuna/OTS_ServerTopLevelAction/OTS_ServerRestrictedTopLevelAction";
+    }
+    
+};
+
+
