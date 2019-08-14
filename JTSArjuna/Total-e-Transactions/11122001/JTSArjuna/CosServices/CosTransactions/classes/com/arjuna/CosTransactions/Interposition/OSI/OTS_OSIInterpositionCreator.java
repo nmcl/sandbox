8c27b33d54eca0fc2d5987804e1207defdf88b81
Mark Little <mlittle@redhat.com>
2019-08-14 15:51:54 -0400

    OSI compliant interposition

---
+++ JTSArjuna/Total-e-Transactions/11122001/JTSArjuna/CosServices/CosTransactions/classes/com/arjuna/CosTransactions/Interposition/OSI/OTS_OSIInterpositionCreator.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 1998, 1999, 2000,
+ *
+ * Arjuna Solutions Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.  
+ *
+ * $Id: OTS_OSIInterpositionCreator.java,v 1.1.26.1 2000/12/04 12:12:08 nmcl Exp $
+ */
+
+package com.arjuna.CosTransactions.Interposition.OSI;
+
+import com.arjuna.CosTransactions.OTS_Control;
+import com.arjuna.CosTransactions.Interposition.OTS_FactoryCreator;
+import org.omg.CosTransactions.PropagationContext;
+import org.omg.CosTransactions.Control;
+
+import org.omg.CORBA.SystemException;
+
+public class OTS_OSIInterpositionCreator implements OTS_FactoryCreator
+{
+
+public OTS_Control recreateLocal (PropagationContext ctx) throws SystemException
+    {
+	return OTS_OSIInterposition.create(ctx);
+    }
+
+public Control recreate (PropagationContext ctx) throws SystemException
+    {
+	return recreateLocal(ctx).getControl();
+    }
+
+};
