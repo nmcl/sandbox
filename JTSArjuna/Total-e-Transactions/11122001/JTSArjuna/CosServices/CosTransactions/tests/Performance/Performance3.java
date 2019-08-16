9456c7aaf31f350a15ed294e7fac0b47ced9d780
Mark Little <mlittle@redhat.com>
2019-08-16 11:47:08 -0400

    some tests

---
+++ JTSArjuna/Total-e-Transactions/11122001/JTSArjuna/CosServices/CosTransactions/tests/Performance/Performance3.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2000,
+ *
+ * Arjuna Solutions Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.
+ *
+ * $Id: Performance3.java,v 1.1.2.1.28.1.116.1 2002/07/10 09:20:01 nmcl Exp $
+ */
+
+import com.arjuna.OrbCommon.*;
+import com.arjuna.CosTransactions.*;
+import org.omg.CosTransactions.*;
+import java.util.*;
+
+public class Performance3
+{
+    
+public static void main (String[] args)
+    {
+	try
+	{
+	    ORBInterface.initORB(args, null);
+	    OAInterface.initOA();
+
+	    double iters = 1000.0;
+	    boolean doCommit = true;
+
+	    for (int i = 0; i < args.length; i++)
+	    {
+		if (args[i].equals("-rollback"))
+		    doCommit = false;
+		else
+		{
+		    if (args[i].equals("-help"))
+		    {
+			System.out.println("Usage: Performance1 [-rollback] [-help]");
+			System.exit(0);
+		    }
+		    else
+		    {
+			System.err.println("Unknown option: "+args[i]);
+			System.exit(0);
+		    }
+		}
+	    }
+	    
+	    OTS_Transaction tx = null;
+	    
+	    // Run ten interations first.
+
+	    for (int i = 0; i < 10; i++)
+	    {
+		tx = new OTS_Transaction((Control) null, (OTS_Transaction) null);
+
+		if (doCommit)
+		    tx.commit(true);
+		else
+		    tx.rollback();
+	    }
+
+	    // Record the start time.
+
+	    Date startTime = new Date();
+
+	    // Run 1000 interations.
+	    
+	    for (int i = 0; i < iters; i++)
+	    {
+		tx = new OTS_Transaction((Control) null, (OTS_Transaction) null);
+
+		if (doCommit)
+		    tx.commit(true);
+		else
+		    tx.rollback();
+	    }
+
+	    // Record the end time.
+
+	    Date endTime = new Date();
+	    double txnTime = (float)((endTime.getTime()-startTime.getTime())/iters);
+	    double txnPSec = 1000.0/txnTime;
+
+	    System.out.println("Using raw transaction objects.\n");
+	    
+	    System.out.println("Average time for empty transaction = "+txnTime);
+	    System.out.println("Transactions per second = "+txnPSec);
+	}
+	catch (Exception e)
+	{
+	    e.printStackTrace();
+	}
+	
+	OAInterface.shutdownOA();
+	ORBInterface.shutdownORB();
+    }
+
+};
