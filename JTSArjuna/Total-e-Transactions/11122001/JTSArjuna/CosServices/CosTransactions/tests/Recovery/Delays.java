96e9feeb9c24d00d93a61425ac8a7f90db000d03
Mark Little <mlittle@redhat.com>
2019-08-16 11:47:43 -0400

    tests

---
+++ JTSArjuna/Total-e-Transactions/11122001/JTSArjuna/CosServices/CosTransactions/tests/Recovery/Delays.java
@@ -0,0 +1,18 @@
+//
+// Copyright (C) 2000,
+//
+// Arjuna Solutions, Ltd.,
+// Newcastle upon Tyne,
+// Tyne and Wear,
+// UK.
+//
+// $Id: Delays.java,v 1.1.2.2 2002/07/04 10:41:29 delsworthy Exp $
+//
+
+public class Delays
+{
+    public static int replayCompletionDelay()
+    {
+        return 60 * 1000;
+    }
+}
