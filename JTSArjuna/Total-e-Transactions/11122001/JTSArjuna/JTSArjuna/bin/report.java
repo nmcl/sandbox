22f1f106f8db69bb3a6f648b157af06859ce11fb
Mark Little <mlittle@redhat.com>
2019-08-17 08:55:28 -0400

    Create report.java

---
+++ JTSArjuna/Total-e-Transactions/11122001/JTSArjuna/JTSArjuna/bin/report.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2000, 2001,
+ *
+ * Arjuna Solutions Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.  
+ * 
+ * $Id: report.java,v 1.1.30.1.4.1 2001/01/11 14:13:20 nmcl Exp $
+ */
+
+import com.arjuna.JTSArjuna.Info;
+import java.io.*;
+
+public class report
+{
+
+public static void main (String[] args)
+    {
+	Info i = new Info();
+
+	System.out.println(i);
+    }
+
+}
