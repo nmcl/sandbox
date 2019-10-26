d308c94b875e9d4bd6762d14a0c4b7290a3f7a95
Mark Little <mlittle@redhat.com>
2019-10-26 12:42:01 -0400

    Create ORBType.java

---
+++ JTSArjuna/Total-e-Transactions/11122001/JTSArjuna/OrbPortability/classes/com/arjuna/orb/ORBType.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 1998, 1999, 2000, 2001,
+ *
+ * Arjuna Solutions Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.
+ *
+ * $Id: ORBType.java,v 1.1.2.1.28.2.2.1.2.1.2.1.90.1 2001/12/11 13:37:20 nmcl Exp $
+ */
+
+package com.arjuna.orb;
+
+/**
+ * The various types of ORB that are supported.
+ *
+ * @author Mark Little (mark@arjuna.com)
+ * @version $Id: ORBType.java,v 1.1.2.1.28.2.2.1.2.1.2.1.90.1 2001/12/11 13:37:20 nmcl Exp $
+ * @since JTS 2.0.
+ */
+
+public class ORBType
+{
+
+public static final int O_SUNSJDK = 0;
+public static final int O_VISIBROKER = 1;
+public static final int O_ORBACUS = 2;
+public static final int O_DAISJ2 = 3;
+public static final int O_VOYAGER = 4;
+public static final int O_ORBIXWEB = 5;
+public static final int O_JBROKER = 6;
+public static final int O_ORBIX2000 = 7;
+public static final int O_JACORB = 8;
+public static final int O_HPORB = 9;
+
+public static final String sun = "SUNSJDK";
+public static final String vbroker = "VISIBROKER";
+public static final String orbacus = "ORBACUS";
+public static final String dais = "DAISJ2";
+public static final String vger = "VOYAGER";
+public static final String orbixweb = "ORBIXWEB";
+public static final String jbroker = "JBROKER";
+public static final String orbix2000 = "ORBIX2000";
+public static final String jacorb = "JACORB";
+public static final String hporb = "HPORB";
+    
+}
