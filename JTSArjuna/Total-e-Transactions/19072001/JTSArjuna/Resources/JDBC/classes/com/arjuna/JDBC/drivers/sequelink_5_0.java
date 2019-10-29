483ea51c3cde5f00a6903ae4f845c7d32745cf09
Mark Little <mlittle@redhat.com>
2019-10-29 06:13:03 -0400

    jdbc

---
+++ JTSArjuna/Total-e-Transactions/19072001/JTSArjuna/Resources/JDBC/classes/com/arjuna/JDBC/drivers/sequelink_5_0.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2000,
+ *
+ * Arjuna Solutions Limited,
+ * Newcastle upon Tyne,
+ * Tyne and Wear,
+ * UK.  
+ *
+ * $Id: sequelink_5_0.java,v 1.1.2.1 2000/11/28 14:50:34 nmcl Exp $
+ */
+
+package com.arjuna.JDBC.drivers;
+
+public class sequelink_5_0
+{
+
+public static final String driverName ()
+    {
+	return "sequelink";
+    }
+
+}
