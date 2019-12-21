e3fc84144e79e11eba89013ce43d228a93af27dc
Mark Little <m.c.little@ncl.ac.uk>
2019-12-21 19:17:50 +0000

    various updates

--- GameOfLife/World.java
+++ GameOfLife/World.java
@@ -129,29 +129,29 @@ class World
 		 * Start by counting the number of live cells around us.
 		 */
 
-		int n1x = x-1
+		int n1x = x-1;
 		int n1y = y-1;
 
 		if ((n1x >= 0) || (n1y >= 0))
-		    liveNeighbours += _theWorld[n1x][n1y];
+		    liveNeighbours += _theWorld[n1x][n1y].isAlive();
 		    
 		int n2x = x-1;
 		int n2y = y;
 
 		if (n2x >= 0)
-		    liveNeighbours += _theWorld[n2x][n2y];
+		    liveNeighbours += _theWorld[n2x][n2y].isAlive();
 		
 		int n3x = x-1;
 		int n3y = y+1;
 
 		if ((n3x >= 0) || (n3y < _y))
-		    liveNeighbours += _theWorld[n3x][n3y];
+		    liveNeighbours += _theWorld[n3x][n3y].isAlive();
 		
 		int n4x = x;
 		int n4y = y-1;
 
 		if (n4y >=0)
-		    liveNeighbours += _theWorld[n4x][n4y];
+		    liveNeighbours += _theWorld[n4x][n4y].isAlive();
 		
 		int n5x = x;
 		int n5y = y;
@@ -162,36 +162,64 @@ class World
 		int n6y = y+1;
 
 		if (n6y < _y)
-		    liveNeighbours += _theWorld[n6x][n6y];
+		    liveNeighbours += _theWorld[n6x][n6y].isAlive();
 
 		int n7x = x+1;
 		int n7y = y-1;
 
 		if ((n7x < _x) || (n7y >= 0))
-		    liveNeighbours += _theWorld[n7x][n7y];
+		    liveNeighbours += _theWorld[n7x][n7y].isAlive();
 		
 		int n8x = x+1;
 		int n8y = y;
 
 		if (n8x < _x)
-		    liveNeighbours += _theWorld[n8x][n8y];
+		    liveNeighbours += _theWorld[n8x][n8y].isAlive();
 		
 		int n9x = x+1;
 		int n9y = y+1;
 
 		if ((n9x < _x) || (n9y < _y))
-		    liveNeighbours += _theWorld[n9x][n9y];
+		    liveNeighbours += _theWorld[n9x][n9y].isAlive();
 
-	/**
-	 * 1) Any live cell with two or three neighbours survives.
-	 * 2) Any dead cell with three live neighbours becomes a live cell.
-	 * 3) All other live cells die in the next generation.
-	 * 4) Dead cells remain dead unless triggered into life by rule 2).
-	 */
+		if (current.isAlive() == 1)
+		{
+		    /**
+		     * 1) Any live cell with two or three neighbours survives.
+		     */
 
+		    if ((liveNeighbours == 2) || (liveNeighbours == 3))
+		    {
+			// do nothing except get older
 
+			nextGeneration[x][y] = new Cell(1, current.getAge());
+		    }
+		}
+		else
+		{
+		    /**
+		     * 2) Any dead cell with three live neighbours becomes a live cell.
+		     */
+
+		    if (liveNeighbours == 3)
+		    {
+			nextGeneration[x][y] = new Cell(1, 1);
+		    }
+		}
+
+		/**
+		 * 3) All other live cells die in the next generation.
+		 * 4) Dead cells remain dead unless triggered into life by rule 2).
+		 */
+
+		if (nextGeneration[x][y] == null)
+		{
+		    nextGeneration[x][y] = new Cell();
+		}
 	    }
 	}
+
+	_theWorld = nextGeneration;
     }
     
     private int _x;
