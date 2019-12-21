07a993b36b5dec5b32fa54022acdf3b43c11cff8
Mark Little <m.c.little@ncl.ac.uk>
2019-12-21 20:00:39 +0000

    updated

--- GameOfLife/ClearScreen.java
+++ GameOfLife/ClearScreen.java
@@ -4,7 +4,10 @@ public class ClearScreen
 {
     static void clrscr ()
     {
-	System.out.print("\033\143");
+	//	System.out.print("\033\143");
+
+	System.out.print("\033[H\033[2J");  
+	System.out.flush();  	
     }
     
     public static void main (String[] args)
