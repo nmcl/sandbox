284cd23bef52bc38fa38f52266312e53db61f0a2
Mark Little <m.c.little@ncl.ac.uk>
2019-12-23 21:07:03 +0000

    added more comments

--- Mandelbrot/Mandelbrot.java
+++ Mandelbrot/Mandelbrot.java
@@ -1,5 +1,20 @@
 public class Mandelbrot
 {
+    /**
+     * This represents the various ASCII characters we will
+     * use to convert the iterations covered in to printable
+     * values.
+     *
+     * In theory we could have to iterate over the function
+     * forever to determine if it is part of the set or not.
+     * Therefore, we place a limit on the number of iterations.
+     * More iterations improves accuracy and if we were displaying
+     * the set graphically we could us colours. Since we are using
+     * ASCII characters that limit us to what we can print, we set
+     * the number of iterations based on the nuumber of characters
+     * from which we have to select, based on the number of iterations.
+     */
+    
     private static String[] values = "0123456789`¬!£$%^&*()_+={}[]:@~;'#<>?,./|qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM".split("");
 
     private static void print (String[][] world, int height, int width)
@@ -17,12 +32,18 @@ public class Mandelbrot
 	    System.out.println();
 	}
     }
+
+    /**
+     * Mandelbrot set history: https://www.scientificamerican.com/article/mandelbrot-set-1990-horgan/
+     *
+     * Or this: http://mathworld.wolfram.com/MandelbrotSet.html
+     */
     
     public static void main (String[] args)
     {
 	int width = 100;
 	int height = 50;
-	int maxIters = values.length;
+	int maxIters = values.length;  // maximum iterations before we decide if value is part of set or not.
 	
 	String[][] world = new String[width][height];
 
@@ -45,10 +66,10 @@ public class Mandelbrot
                     iters++;
                 }
 		
-                if (iters == maxIters)
+                if (iters == maxIters)  // not part of the set
 		    world[col][row] = "-";
                 else
-		    world[col][row] = values[iters];
+		    world[col][row] = values[iters];  // part of the set
             }
         }
 
