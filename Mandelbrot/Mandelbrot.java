2bb46dc30e30d7b99af1603276ac3e8488811e9d
Mark Little <m.c.little@ncl.ac.uk>
2019-12-23 20:53:07 +0000

    added print

--- Mandelbrot/Mandelbrot.java
+++ Mandelbrot/Mandelbrot.java
@@ -2,14 +2,26 @@ public class Mandelbrot
 {
     private static String[] values = "0123456789`¬!£$%^&*()_+={}[]:@~;'#<>?,./|qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM".split("");
 
-    private static void print (String[][] world)
+    private static void print (String[][] world, int height, int width)
     {
+	System.out.print("\033[H\033[2J");
+	System.out.flush();
+
+        for (int row = 0; row < height; row++)
+	{
+            for (int col = 0; col < width; col++)
+	    {
+		System.out.print(world[col][row]);
+	    }
+
+	    System.out.println();
+	}
     }
     
     public static void main (String[] args)
     {
-	int width = 1000;
-	int height = 1080;
+	int width = 100;
+	int height = 50;
 	int maxIters = values.length;
 	
 	String[][] world = new String[width][height];
@@ -39,5 +51,7 @@ public class Mandelbrot
 		    world[col][row] = values[iters];
             }
         }
+
+	print(world, height, width);
     }
 }
